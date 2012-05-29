5f2f6b9c7386c7cc410fd70c0ab45cbd1ee38e3f
mlyly <mlyly@15ac3db4-bff1-4aba-a278-c1783e67ca60>
2012-05-29 05:58:51 +0000

    OVT-672: Perustoteutus webapp / portlet applikaatiolle

--- tarjonta-service/src/main/java/fi/vm/sade/tarjonta/dao/KoulutusmoduuliDAO.java
+++ tarjonta-service/src/main/java/fi/vm/sade/tarjonta/dao/KoulutusmoduuliDAO.java
@@ -1,13 +1,13 @@
 /*
  * Copyright (c) 2012 The Finnish Board of Education - Opetushallitus
- * 
+ *
  * This program is free software:  Licensed under the EUPL, Version 1.1 or - as
  * soon as they will be approved by the European Commission - subsequent versions
  * of the EUPL (the "Licence");
- * 
+ *
  * You may not use this work except in compliance with the Licence.
  * You may obtain a copy of the Licence at: http://www.osor.eu/eupl/
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
@@ -23,7 +23,6 @@ import fi.vm.sade.tarjonta.model.Koulutusmoduuli;
  * @author Jukka Raanamo
  */
 public interface KoulutusmoduuliDAO extends JpaDAO<Koulutusmoduuli, Long> {
-    
-    
-}
 
+
+}
