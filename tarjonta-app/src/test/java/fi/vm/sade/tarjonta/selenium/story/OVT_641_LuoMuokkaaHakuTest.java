b44efd73fa25d839a7934cbbb92d0aab381934ae
salonean <salonean@15ac3db4-bff1-4aba-a278-c1783e67ca60>
2012-06-13 12:00:04 +0000

    OVT-798 hakurä gui validoinnit

--- tarjonta-app/src/test/java/fi/vm/sade/tarjonta/selenium/story/OVT_641_LuoMuokkaaHakuTest.java
+++ tarjonta-app/src/test/java/fi/vm/sade/tarjonta/selenium/story/OVT_641_LuoMuokkaaHakuTest.java
@@ -14,6 +14,7 @@ import org.openqa.selenium.By;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import static org.junit.Assert.*;
+import static fi.vm.sade.support.selenium.SeleniumUtils.*;
 
 /**
  * Test for HakueraEditForm.  
