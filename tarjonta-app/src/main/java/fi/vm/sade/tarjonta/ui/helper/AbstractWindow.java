559bc4e0da4a3df6dabcef906dbc095e8493c866
mlyly <mlyly@15ac3db4-bff1-4aba-a278-c1783e67ca60>
2012-09-24 07:37:27 +0000

    OVT-2043 Projektin perustan rakennusta

---
+++ tarjonta-app/src/main/java/fi/vm/sade/tarjonta/ui/helper/AbstractWindow.java
@@ -0,0 +1,76 @@
+package fi.vm.sade.tarjonta.ui.helper;
+
+import com.vaadin.ui.VerticalLayout;
+import com.vaadin.ui.Window;
+import com.vaadin.ui.Window.CloseEvent;
+
+/**
+ *
+ * @author jani
+ */
+public abstract class AbstractWindow extends Window implements Window.CloseListener {
+
+    private VerticalLayout windowLayout;
+
+    public AbstractWindow(String label) {
+        super(label);
+        init("75%", ComponentUtil.DEFAULT_REALTIVE_SIZE, true, null, true);
+    }
+
+    public AbstractWindow(String label, boolean doLayout) {
+        super(label);
+       init("75%", ComponentUtil.DEFAULT_REALTIVE_SIZE, true, null, doLayout);
+    }
+
+    public AbstractWindow(String label, String width, String height, boolean modal) {
+        super(label);
+        init(width, height, modal, null, true);
+    }
+
+    public AbstractWindow(String label, String width, String height, boolean modal, VerticalLayout layout) {
+        super(label);
+        init(width, height, modal, layout, true);
+    }
+
+    protected void init(String width, String height, boolean modal, VerticalLayout layout, boolean doLayout) {
+        ComponentUtil.handleWidth(this, width);
+        ComponentUtil.handleHeight(this, height);
+        center();
+        setModal(true);
+
+        if (layout == null) {
+            windowLayout =  UiBuilder.newVerticalLayout(true, UiMarginEnum.NONE);
+        } else {
+            windowLayout =  layout;
+        }
+
+        windowLayout.setSizeFull();
+        this.setContent(windowLayout);
+
+        if (doLayout) {
+            buildOrder(windowLayout);
+        }
+    }
+
+    protected void buildOrder(VerticalLayout layout) {
+        buildLayout(layout);
+    }
+
+    public abstract void buildLayout(VerticalLayout layout);
+
+    @Override
+    public void windowClose(CloseEvent e) {
+        throw new UnsupportedOperationException("Not implemented yet.");
+    }
+
+    public void fullContentHeight() {
+        getContent().setHeight("100%");
+    }
+
+    public VerticalLayout getLayout() {
+        return windowLayout;
+    }
+
+    public void test() {
+    }
+}
