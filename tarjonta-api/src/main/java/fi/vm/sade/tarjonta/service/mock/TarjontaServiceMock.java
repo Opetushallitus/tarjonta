84db61ae1ba7eff11d8ad7cc06d0ec6cfe18e4cd
kkammonen <kkammonen@15ac3db4-bff1-4aba-a278-c1783e67ca60>
2012-09-25 10:32:48 +0000

    Tarjonta rajapinta sijoittelun käyttöön

--- tarjonta-api/src/main/java/fi/vm/sade/tarjonta/service/mock/TarjontaServiceMock.java
+++ tarjonta-api/src/main/java/fi/vm/sade/tarjonta/service/mock/TarjontaServiceMock.java
@@ -3,19 +3,9 @@ package fi.vm.sade.tarjonta.service.mock;
 import fi.vm.sade.tarjonta.service.TarjontaService;
 import fi.vm.sade.tarjonta.service.types.ListHakuVastausTyyppi;
 import fi.vm.sade.tarjonta.service.types.ListaaHakuTyyppi;
-import fi.vm.sade.tarjonta.service.types.TarjontaTyyppi;
+import fi.vm.sade.tarjonta.service.types.tarjonta.TarjontaTyyppi;
 
-
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Date;
-import java.util.GregorianCalendar;
-import java.util.List;
-
-import javax.xml.datatype.DatatypeFactory;
-import javax.xml.datatype.XMLGregorianCalendar;
-
-
+ 
 public class TarjontaServiceMock implements TarjontaService {
 
     @Override
