629cc464f4a09536134a480de5d796068151303a
tkatva <tkatva@15ac3db4-bff1-4aba-a278-c1783e67ca60>
2012-09-28 09:48:01 +0000

    OVT-2040

--- tarjonta-api/src/main/java/fi/vm/sade/tarjonta/service/mock/TarjontaServiceMock.java
+++ tarjonta-api/src/main/java/fi/vm/sade/tarjonta/service/mock/TarjontaServiceMock.java
@@ -4,36 +4,92 @@ import fi.vm.sade.tarjonta.service.TarjontaService;
 import fi.vm.sade.tarjonta.service.types.ListHakuVastausTyyppi;
 import fi.vm.sade.tarjonta.service.types.ListaaHakuTyyppi;
 import fi.vm.sade.tarjonta.service.types.tarjonta.Haku;
+import fi.vm.sade.tarjonta.service.types.tarjonta.HaunNimi;
 import fi.vm.sade.tarjonta.service.types.tarjonta.TarjontaTyyppi;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.UUID;
 
  
 public class TarjontaServiceMock implements TarjontaService {
 
+    private HashMap<String,Haku> haut = new HashMap<String, Haku>();
+    
+    public TarjontaServiceMock() {
+        initDefValues();
+    }
+    
     @Override
     public ListHakuVastausTyyppi listHaku(ListaaHakuTyyppi parameters) {
-        // TODO Auto-generated method stub
-        return null;
+        ListHakuVastausTyyppi lvt = new ListHakuVastausTyyppi();
+        lvt.getResponse().addAll(haut.values());
+        return lvt;
     } 
 
     @Override
     public TarjontaTyyppi haeTarjonta(String oid) {
-        // TODO Auto-generated method stub
-        return null;
+        TarjontaTyyppi tt = new TarjontaTyyppi();
+        tt.setHaku(haut.get(oid));
+        return tt;
     }
 
     @Override
     public Haku paivitaHaku(Haku hakuDto) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        haut.remove(hakuDto.getOid());
+        haut.put(hakuDto.getOid(), hakuDto);
+        return hakuDto;
     }
 
     @Override
     public Haku lisaaHaku(Haku hakuDto) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        haut.put(hakuDto.getOid(), hakuDto);
+        return hakuDto;
     }
 
     @Override
     public void poistaHaku(Haku hakuDto) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        haut.remove(hakuDto.getOid());
+    }
+    
+    
+    private void initDefValues() {
+        Haku haku = new Haku();
+        HaunNimi nimi = new HaunNimi();
+        nimi.setKielikoodi("fi");
+        nimi.setNimi("Yhteishaku syksy 2013");
+        haku.getHaunKielistetytNimet().add(nimi);
+        HaunNimi nimiSe = new HaunNimi();
+        nimiSe.setKielikoodi("se");
+        nimiSe.setNimi("gemensam sökning höst 2013");
+        haku.getHaunKielistetytNimet().add(nimiSe);
+        haku.setOid(UUID.randomUUID().toString());
+        haku.setHakuVuosi(2013);
+        haku.setHaunAlkamisPvm(new Date());
+        haku.setHaunLoppumisPvm(new Date(System.currentTimeMillis() + 100000));
+        haku.setSijoittelu(true);
+        haku.setHaunTunniste("123456");
+        haku.setKoulutuksenAlkamisVuosi(2014);
+       
+        haut.put(haku.getOid(), haku);
+        
+        Haku haku2 = new Haku();
+        HaunNimi nimiFi = new HaunNimi();
+        nimiFi.setKielikoodi("fi");
+        nimiFi.setNimi("Yhteishaku syksy 2014");
+        haku2.getHaunKielistetytNimet().add(nimiFi);
+        HaunNimi nimiSee = new HaunNimi();
+        nimiSee.setKielikoodi("se");
+        nimiSee.setNimi("gemensam sökning höst 2014");
+        haku2.getHaunKielistetytNimet().add(nimiSee);
+        haku2.setOid(UUID.randomUUID().toString());
+        haku2.setHakuVuosi(2014);
+        haku2.setHaunAlkamisPvm(new Date());
+        haku2.setHaunLoppumisPvm(new Date(System.currentTimeMillis() + 100000));
+        haku2.setSijoittelu(true);
+        haku2.setHaunTunniste("7891011");
+        haku2.setKoulutuksenAlkamisVuosi(2015);
+        
+        haut.put(haku2.getOid(), haku2);
     }
 
 }
