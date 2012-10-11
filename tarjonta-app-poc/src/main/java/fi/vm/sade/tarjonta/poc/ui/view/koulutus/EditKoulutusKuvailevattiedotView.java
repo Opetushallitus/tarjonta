582cb954740ebbf489c1ac45952abaf71d45c2a8
mlyly <mlyly@15ac3db4-bff1-4aba-a278-c1783e67ca60>
2012-10-11 10:51:14 +0000

    Vaihdettu RichTextArea TinyMCE editoriksi - testailua.

--- tarjonta-app-poc/src/main/java/fi/vm/sade/tarjonta/poc/ui/view/koulutus/EditKoulutusKuvailevattiedotView.java
+++ tarjonta-app-poc/src/main/java/fi/vm/sade/tarjonta/poc/ui/view/koulutus/EditKoulutusKuvailevattiedotView.java
@@ -15,21 +15,14 @@
  */
 package fi.vm.sade.tarjonta.poc.ui.view.koulutus;
 
-import com.vaadin.ui.AbstractLayout;
-import com.vaadin.ui.Button;
-import com.vaadin.ui.Button.ClickEvent;
-import com.vaadin.ui.HorizontalLayout;
-import com.vaadin.ui.Panel;
-import com.vaadin.ui.RichTextArea;
+import com.vaadin.ui.Component;
 import com.vaadin.ui.TabSheet;
-import com.vaadin.ui.VerticalLayout;
 import fi.vm.sade.tarjonta.poc.ui.TarjontaPresenter;
-import fi.vm.sade.tarjonta.poc.ui.enums.Notification;
 import fi.vm.sade.tarjonta.poc.ui.view.common.AutoSizeVerticalLayout;
-import fi.vm.sade.vaadin.constants.UiMarginEnum;
 import fi.vm.sade.vaadin.util.UiUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Configurable;
+import org.vaadin.tinymceeditor.TinyMCETextField;
 
 /**
  *
@@ -72,10 +65,32 @@ public class EditKoulutusKuvailevattiedotView extends AutoSizeVerticalLayout {
             tabs.addTab(createRTA(), "Englanti");
         }
     }
-    private RichTextArea createRTA() {
-        RichTextArea rta = UiUtil.richTextArea(null, null, null);
-        rta.setWidth(100, UNITS_PERCENTAGE);
-        rta.setValue("ed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?");
-        return rta;
+    private Component createRTA() {
+
+        TinyMCETextField tinyTF = new TinyMCETextField();
+        tinyTF.setValue("<p><b>This</b> <i>text</i> is <u>cool</u>!</p>\n" +
+                "<ul>\n" +
+                "<li>Row 1</li>\n" +
+                "<li>Row 2</li>\n" +
+                "<li>Row three</li>\n" +
+                "</ul>\n" +
+                "<ol>\n" +
+                "<li>One</li>\n" +
+                "<li>Two</li>\n" +
+                "<li>Three</li>\n" +
+                "</ol>\n" +
+                "<p>&nbsp;</p>");
+        tinyTF.setWidth("400px");
+        tinyTF.setHeight("150px");
+
+        tinyTF.setCaption("TinyMCE via Vaadin Wrapper Addon - simple mode");
+        tinyTF.setConfig("{theme: 'simple'}");
+
+        return tinyTF;
+
+//        RichTextArea rta = UiUtil.richTextArea(null, null, null);
+//        rta.setWidth(100, UNITS_PERCENTAGE);
+//        rta.setValue("ed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?");
+//        return rta;
     }
 }
