07b91338b78eb49d887726a050dc4a03e9068fed
jwilen <jwilen@15ac3db4-bff1-4aba-a278-c1783e67ca60>
2012-11-02 13:35:56 +0000

    OVT-2583, korjaus: KOMO:n ja KOMOTO:n datan tarkastus yhdenmukaistettu.

--- tarjonta-service/src/main/java/fi/vm/sade/tarjonta/service/business/impl/KoulutusBusinessServiceImpl.java
+++ tarjonta-service/src/main/java/fi/vm/sade/tarjonta/service/business/impl/KoulutusBusinessServiceImpl.java
@@ -24,6 +24,8 @@ import fi.vm.sade.tarjonta.service.business.KoulutusBusinessService;
 import fi.vm.sade.tarjonta.service.business.exception.TarjontaBusinessException;
 import fi.vm.sade.tarjonta.service.types.LisaaKoulutusTyyppi;
 import fi.vm.sade.tarjonta.service.types.PaivitaKoulutusTyyppi;
+import fi.vm.sade.tarjonta.service.types.koodisto.Nimi;
+import fi.vm.sade.tarjonta.service.types.tarjonta.KoodistoKoodiTyyppi;
 import fi.vm.sade.tarjonta.service.types.tarjonta.TarjontaVirheKoodi;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -35,79 +37,89 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 @Transactional
 public class KoulutusBusinessServiceImpl implements KoulutusBusinessService {
-    
+
     @Autowired
     private KoulutusmoduuliDAO koulutusmoduuliDAO;
     @Autowired
     private KoulutusmoduuliToteutusDAO koulutusmoduuliToteutusDAO;
     @Autowired
     private KoulutusSisaltyvyysDAO sisaltyvyysDAO;
-    
+
     @Override
     public Koulutusmoduuli create(Koulutusmoduuli moduuli) {
-        
+
         return koulutusmoduuliDAO.insert(moduuli);
-        
+
     }
-    
+
     @Override
     public KoulutusmoduuliToteutus create(KoulutusmoduuliToteutus toteutus, Koulutusmoduuli moduuli) {
-        
+
         final Koulutusmoduuli m = isNew(moduuli) ? create(moduuli) : moduuli;
         toteutus.setKoulutusmoduuli(m);
-        
+
         return (KoulutusmoduuliToteutus) koulutusmoduuliToteutusDAO.insert(toteutus);
-        
+
     }
-    
+
     @Override
     public Koulutusmoduuli findTutkintoOhjelma(String koulutusLuokitusUri, String koulutusOhjelmaUri) {
 
         // todo: dao kerroksen voisi poistaa, ainoastaan vaikeammat haut voisi sijoittaa helper:n taakse
 
         return koulutusmoduuliDAO.findTutkintoOhjelma(koulutusLuokitusUri, koulutusOhjelmaUri);
-        
+
     }
-    
+
     @Override
     public KoulutusmoduuliToteutus createKoulutus(LisaaKoulutusTyyppi koulutus) {
-        
-        
+
+
         Koulutusmoduuli moduuli = koulutusmoduuliDAO.findTutkintoOhjelma(
                 koulutus.getKoulutusKoodi().getUri(),
                 koulutus.getKoulutusohjelmaKoodi().getUri());
-        
+
         if (moduuli == null) {
             throw new TarjontaBusinessException(TarjontaVirheKoodi.KOULUTUSTA_EI_OLEMASSA.value());
         }
-        
+
         KoulutusmoduuliToteutus komotoModel = new KoulutusmoduuliToteutus();
         EntityUtils.copyFields(koulutus, komotoModel);
         komotoModel.setKoulutusmoduuli(moduuli);
         moduuli.addKoulutusmoduuliToteutus(komotoModel);
         
+        
+        //set all search keywords
+        StringBuilder keywords = new StringBuilder();
+        for (KoodistoKoodiTyyppi.Nimi nimi : koulutus.getKoulutusKoodi().getNimi()) {
+            keywords.append(nimi.getValue()).append(", ");
+        }
+        keywords.append(koulutus.getKoulutusKoodi().getArvo());
+
+        komotoModel.setNimi(keywords.toString());
+
         return koulutusmoduuliToteutusDAO.insert(komotoModel);
-        
-        
+
+
     }
-    
+
     @Override
     public KoulutusmoduuliToteutus updateKoulutus(PaivitaKoulutusTyyppi koulutus) {
-        
+
         final String oid = koulutus.getOid();
         KoulutusmoduuliToteutus model = koulutusmoduuliToteutusDAO.findByOid(oid);
-        
+
         if (model == null) {
             throw new TarjontaBusinessException(TarjontaVirheKoodi.OID_EI_OLEMASSA.value(), oid);
         }
-        
+
         EntityUtils.copyFields(koulutus, model);
         koulutusmoduuliToteutusDAO.update(model);
-        
+
         return model;
-        
+
     }
-    
+
     private boolean isNew(BaseEntity e) {
         // no good
         return (e.getId() == null);
