60611c698b6ca0863048f3f1038e0934c4fcbcd0
jraanamo <jraanamo@15ac3db4-bff1-4aba-a278-c1783e67ca60>
2012-11-12 07:59:41 +0000

    OVT-2593: korjattu xs:dataTime mäppäys binding.xjb:ssä käyttämään dateTime parsintaa date:n parsinnan sijaan

--- tarjonta-api/src/main/java/fi/vm/sade/tarjonta/service/helper/DateHelper.java
+++ tarjonta-api/src/main/java/fi/vm/sade/tarjonta/service/helper/DateHelper.java
@@ -14,48 +14,57 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * European Union Public Licence for more details.
  */
-
 package fi.vm.sade.tarjonta.service.helper;
 
 import java.util.Calendar;
 import java.util.Date;
 import javax.xml.bind.DatatypeConverter;
 
-
-
-
 /**
  *
  * @author Tuomas Katva
  */
 public class DateHelper {
-      /**
-	 * Converts a String representing a {@link XMLGregorianCalendar} to a
-	 * {@link Date}.
-	 * 
-	 * @param xgc
-	 *            String to convert from
-	 * @return Computed date
-	 */
-	public static Date parseDate(String xgc) {
 
-		return DatatypeConverter.parseDate(xgc).getTime();
-	}
+    /**
+     * Converts a String representing a {@link XMLGregorianCalendar} to a
+     * {@link Date}.
+     *
+     * @param xgc
+     *            String to convert from
+     * @return Computed date
+     */
+    public static Date parseDate(String xgc) {
 
-	/**
-	 * Converts a {@link Date} to a String representing a
-	 * {@link XMLGregorianCalendar}.
-	 * 
-	 * @param date
-	 *            Date to convert from
-	 * @return String representation
-	 */
-	public static String printDate(Date date) {
+        return DatatypeConverter.parseDate(xgc).getTime();
+    }
 
-		Calendar calendar = Calendar.getInstance();
+    /**
+     * Converts a {@link Date} to a String representing a
+     * {@link XMLGregorianCalendar}.
+     *
+     * @param date
+     *            Date to convert from
+     * @return String representation
+     */
+    public static String printDate(Date date) {
 
-		calendar.setTime(date);
+        Calendar calendar = Calendar.getInstance();
 
-		return DatatypeConverter.printDate(calendar);
-	}
+        calendar.setTime(date);
+
+        return DatatypeConverter.printDate(calendar);
+    }
+
+    public static Date parseDateTime(String xmlDateTime) {
+        return DatatypeConverter.parseDateTime(xmlDateTime).getTime();
+    }
+
+    public static String printDateTime(Date date) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        return DatatypeConverter.printDateTime(calendar);
+    }
+
 }
+
