3bd3f9d6f7862a0ce489d6ecee60195cbbaf922d
jraanamo <jraanamo@15ac3db4-bff1-4aba-a278-c1783e67ca60>
2012-11-12 11:23:18 +0000

    OVT-2593: korjattu date/datetime formatointi, lisätty yksikkötestejä

--- tarjonta-publication-api/src/main/java/fi/vm/sade/tarjonta/publication/util/ConverterUtils.java
+++ tarjonta-publication-api/src/main/java/fi/vm/sade/tarjonta/publication/util/ConverterUtils.java
@@ -4,6 +4,12 @@ import java.util.Calendar;
 import java.util.Date;
 import javax.xml.bind.DatatypeConverter;
 
+/**
+ * Due to a features of JAXB this class will be eventually generated into org.w3c._2001.xmlschema -package. Meaning
+ * that any other project creating the similar type converters might result overriding this functionality so be
+ * careful.
+ *
+ */
 public class ConverterUtils {
 
     public static Date parseDate(String xmlDate) {
@@ -21,9 +27,11 @@ public class ConverterUtils {
     }
 
     public static String printDateTime(Date date) {
+
         Calendar calendar = Calendar.getInstance();
         calendar.setTime(date);
         return DatatypeConverter.printDateTime(calendar);
+
     }
 
 }
