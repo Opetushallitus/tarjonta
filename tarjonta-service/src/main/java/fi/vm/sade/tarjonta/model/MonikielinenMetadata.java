2cddd0d2c7a0d1da40b773b3cae7a60bde97d810
mlyly <mlyly@15ac3db4-bff1-4aba-a278-c1783e67ca60>
2013-02-12 08:07:03 +0000

    KJOH-136 Domain malliin monikielinen metadata + dao + daon test case

---
+++ tarjonta-service/src/main/java/fi/vm/sade/tarjonta/model/MonikielinenMetadata.java
@@ -0,0 +1,78 @@
+package fi.vm.sade.tarjonta.model;
+
+import fi.vm.sade.generic.model.BaseEntity;
+
+import javax.persistence.*;
+import java.util.Date;
+
+/**
+ * @author mlyly
+ */
+@Entity
+@Table(name = "monikielinen_metadata", uniqueConstraints = {
+        @UniqueConstraint(columnNames = {"avain", "kategoria", "kieli"})
+})
+public class MonikielinenMetadata extends BaseEntity {
+
+    // Make translated texts behave more like "metadata".
+    // For example: "uri: Sosiaali- ja Terveysala"
+    private String avain = null;
+
+    // For example: "SORA", "Valintaperustekuvaus"
+    private String kategoria = null;
+
+    // For example: "uri: Finnish 358#1"
+    private String kieli = null;
+
+    @Lob
+    @Basic(fetch = FetchType.EAGER)
+    private String arvo = null;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(updatable = false, nullable = false)
+    private Date created = new Date();
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(updatable = true, nullable = false)
+    private Date modified = new Date();
+
+    public String getAvain() {
+        return avain;
+    }
+
+    public void setAvain(String avain) {
+        this.avain = avain;
+    }
+
+    public String getKategoria() {
+        return kategoria;
+    }
+
+    public void setKategoria(String kategoria) {
+        this.kategoria = kategoria;
+    }
+
+    public String getKieli() {
+        return kieli;
+    }
+
+    public void setKieli(String kieli) {
+        this.kieli = kieli;
+    }
+
+    public String getArvo() {
+        return arvo;
+    }
+
+    public void setArvo(String arvo) {
+        this.arvo = arvo;
+    }
+
+    public Date getCreated() {
+        return created;
+    }
+
+    public Date getModified() {
+        return modified;
+    }
+}
