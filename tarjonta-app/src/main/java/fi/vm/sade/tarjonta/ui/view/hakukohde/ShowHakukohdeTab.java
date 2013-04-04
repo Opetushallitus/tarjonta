670ab93c4a748562137f4f5e4e606653b6a1817d
Tuomas Katva <tuomas.katva@gmailcom>
2013-04-04 16:52:34 +0300

    KJOH-339

--- tarjonta-app/src/main/java/fi/vm/sade/tarjonta/ui/view/hakukohde/ShowHakukohdeTab.java
+++ tarjonta-app/src/main/java/fi/vm/sade/tarjonta/ui/view/hakukohde/ShowHakukohdeTab.java
@@ -1,6 +1,7 @@
 package fi.vm.sade.tarjonta.ui.view.hakukohde;
 
 import com.google.common.base.Preconditions;
+import com.vaadin.data.util.BeanContainer;
 import com.vaadin.ui.*;
 import fi.vm.sade.generic.common.I18N;
 import fi.vm.sade.generic.common.I18NHelper;
@@ -9,8 +10,11 @@ import fi.vm.sade.tarjonta.ui.helper.TarjontaUIHelper;
 import fi.vm.sade.tarjonta.ui.helper.UiBuilder;
 import fi.vm.sade.tarjonta.ui.model.HakuViewModel;
 import fi.vm.sade.tarjonta.ui.model.KielikaannosViewModel;
+import fi.vm.sade.tarjonta.ui.model.ValintakoeAikaViewModel;
+import fi.vm.sade.tarjonta.ui.model.ValintakoeViewModel;
 import fi.vm.sade.tarjonta.ui.presenter.TarjontaPresenter;
 import fi.vm.sade.tarjonta.ui.service.OrganisaatioContext;
+import fi.vm.sade.tarjonta.ui.view.hakukohde.tabs.ShowHakukohdeValintakoeRow;
 import fi.vm.sade.vaadin.constants.UiMarginEnum;
 import fi.vm.sade.vaadin.Oph;
 import fi.vm.sade.vaadin.util.UiUtil;
@@ -51,8 +55,100 @@ public class ShowHakukohdeTab extends CustomComponent {
 
     private void buildPage(VerticalLayout layout) {
          buildPerustiedotLayout(layout);
+         addLayoutSplit(layout);
+         buildValintakokeetLayout(layout);
+
+    }
+
+    private void buildValintakokeetLayout(VerticalLayout layout) {
+
+        layout.addComponent(buildHeaderLayout(this.i18n.getMessage("valintakokeetTitle"),i18n.getMessage(CommonTranslationKeys.MUOKKAA),
+                new Button.ClickListener() {
+                    @Override
+                    public void buttonClick(Button.ClickEvent clickEvent) {
+
+                    }
+                }
+                ,presenter.getPermission().userCanUpdateHakukohde(context)));
+
+        VerticalLayout yetAnotherLayout = new VerticalLayout();
+        yetAnotherLayout.setMargin(true);
+
+        for (ValintakoeViewModel valintakoe:presenter.loadHakukohdeValintaKokees()) {
+
+            if (checkValintakoeKieli(valintakoe)) {
+
+            Label titleLabel = UiUtil.label(null,i18n.getMessage("valintakoeTitle"));
+            titleLabel.setStyleName(Oph.LABEL_H2);
+            yetAnotherLayout.addComponent(titleLabel);
+
+            Label valintaKoeTiedot = new Label(getLanguageString(valintakoe.getSanallisetKuvaukset()));
+            valintaKoeTiedot.setContentMode(Label.CONTENT_XHTML);
+            yetAnotherLayout.addComponent(valintaKoeTiedot);
+
+            Label valintakoeAikaTitle = UiUtil.label(null,i18n.getMessage("valintakoeAjatTitle"));
+            valintakoeAikaTitle.setStyleName(Oph.LABEL_H2);
+            yetAnotherLayout.addComponent(valintakoeAikaTitle);
+            yetAnotherLayout.addComponent(buildValintakoeAikaTable(valintakoe));
+
+            String lisanayttoKuvaus = getLanguageString(valintakoe.getLisanayttoKuvaukset());
+            if (lisanayttoKuvaus != null && lisanayttoKuvaus.trim().length() > 0) {
+            Label lisanaytotLbl = UiUtil.label(null,i18n.getMessage("lisanaytotLabel"));
+            lisanaytotLbl.setStyleName(Oph.LABEL_H2);
+            yetAnotherLayout.addComponent(lisanaytotLbl);
+
+            Label lisaNayttoLbl = new Label(getLanguageString(valintakoe.getLisanayttoKuvaukset()));
+            lisaNayttoLbl.setContentMode(Label.CONTENT_XHTML);
+            yetAnotherLayout.addComponent(lisaNayttoLbl);
+            }
+
+            }
+        }
+
+        layout.addComponent(yetAnotherLayout);
 
 
+    }
+
+    private boolean checkValintakoeKieli(ValintakoeViewModel valintakoeViewModel) {
+
+
+        for (KielikaannosViewModel kieli: valintakoeViewModel.getSanallisetKuvaukset()) {
+            if (kieli.getKielikoodi().trim().equalsIgnoreCase(this.language)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private Table buildValintakoeAikaTable(ValintakoeViewModel valintakoe) {
+        Table valintakoeAikaTable = new Table();
+
+        valintakoeAikaTable.setContainerDataSource(createBeanContainer(valintakoe.getValintakoeAjat()));
+        valintakoeAikaTable.setWidth(100,UNITS_PERCENTAGE);
+        valintakoeAikaTable.setVisibleColumns(new String[]{"valintakoeSijainti","valintakoeAika","valintakoeLisatiedot"});
+        valintakoeAikaTable.setColumnHeader("valintakoeSijainti",this.i18n.getMessage("tableValintakoeAikaSijainti"));
+        valintakoeAikaTable.setColumnHeader("valintakoeAika",this.i18n.getMessage("tableValintakoeAikaAjankohta"));
+        valintakoeAikaTable.setColumnHeader("valintakoeLisatiedot",this.i18n.getMessage("tableValintakoeAikaLisatietoja"));
+        valintakoeAikaTable.setColumnExpandRatio("valintakoeSijainti",40);
+        valintakoeAikaTable.setColumnExpandRatio("valintakoeAika",30);
+        valintakoeAikaTable.setColumnExpandRatio("valintakoeLisatiedot",30);
+        valintakoeAikaTable.setPageLength(valintakoe.getValintakoeAjat().size());
+
+        return valintakoeAikaTable;
+    }
+
+    private BeanContainer<String, ShowHakukohdeValintakoeRow> createBeanContainer(List<ValintakoeAikaViewModel> valintaAikas) {
+        BeanContainer<String, ShowHakukohdeValintakoeRow> container = new BeanContainer<String, ShowHakukohdeValintakoeRow>(ShowHakukohdeValintakoeRow.class);
+
+        for (ValintakoeAikaViewModel valintakoeAika:valintaAikas) {
+            ShowHakukohdeValintakoeRow row = new ShowHakukohdeValintakoeRow(valintakoeAika,this.language);
+            container.addItem(new Integer(row.hashCode()).toString(),row);
+
+        }
+
+        return container;
     }
 
     private void buildPerustiedotLayout(VerticalLayout layout) {
@@ -63,7 +159,7 @@ public class ShowHakukohdeTab extends CustomComponent {
 
                      }
                  }
-                 ,presenter.getPermission().userCanCreateHakukohde(context)));
+                 ,presenter.getPermission().userCanUpdateHakukohde(context)));
 
         final GridLayout grid = new GridLayout(2, 1);
         grid.setWidth("100%");
@@ -74,7 +170,8 @@ public class ShowHakukohdeTab extends CustomComponent {
         addItemToGrid(grid,"valinnoissaKaytettavatAloituspaikat",new Integer(presenter.getModel().getHakukohde().getValinnoissaKaytettavatPaikat()).toString());
         addRichTextToGrid(grid,"hakukelpoisuusVaatimukset",getLanguageString(presenter.getModel().getHakukohde().getValintaPerusteidenKuvaus()));
         addRichTextToGrid(grid,"lisatietojaHakemisesta",getLanguageString(presenter.getModel().getHakukohde().getLisatiedot()));
-
+        //grid.setColumnExpandRatio(0,0.2f);
+        grid.setColumnExpandRatio(1,1f);
 
         layout.addComponent(grid);
         layout.setComponentAlignment(grid, Alignment.TOP_LEFT);
@@ -88,8 +185,11 @@ public class ShowHakukohdeTab extends CustomComponent {
 
     private void addRichTextToGrid(final GridLayout grid,
                                final String labelCaptionKey, final String labelCaptionValue) {
+
+
         Label lbl = new Label(labelCaptionValue);
         lbl.setContentMode(Label.CONTENT_XHTML);
+
         addItemToGrid(grid, labelCaptionKey, lbl);
     }
 
@@ -104,6 +204,7 @@ public class ShowHakukohdeTab extends CustomComponent {
 
             final HorizontalLayout textArea = UiUtil.horizontalLayout(false,
                     UiMarginEnum.NONE);
+
             textArea.addComponent(component);
             grid.addComponent(textArea);
 
