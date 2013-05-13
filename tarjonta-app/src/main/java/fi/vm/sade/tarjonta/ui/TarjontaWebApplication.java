1e364819910d30a442788dbe91dd3e35f7154a9d
Sami Siren <sami.siren@proactum.fi>
2013-05-13 12:06:33 +0300

    add buttons for indexing

--- tarjonta-app/src/main/java/fi/vm/sade/tarjonta/ui/TarjontaWebApplication.java
+++ tarjonta-app/src/main/java/fi/vm/sade/tarjonta/ui/TarjontaWebApplication.java
@@ -15,19 +15,20 @@
  */
 package fi.vm.sade.tarjonta.ui;
 
+
 import com.vaadin.ui.Button;
 import com.vaadin.ui.Button.ClickEvent;
 import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.Window;
 
 import fi.vm.sade.tarjonta.service.TarjontaAdminService;
-import fi.vm.sade.tarjonta.ui.enums.KoulutusActiveTab;
 import fi.vm.sade.tarjonta.ui.loader.xls.TarjontaKomoData;
 import fi.vm.sade.tarjonta.ui.view.HakuRootView;
 import fi.vm.sade.tarjonta.ui.view.TarjontaRootView;
 import fi.vm.sade.tarjonta.ui.view.ValintaperustekuvausRootView;
-import fi.vm.sade.tarjonta.ui.view.koulutus.lukio.EditLukioKoulutusKuvailevatTiedotView;
-import fi.vm.sade.tarjonta.ui.view.koulutus.lukio.EditLukioKoulutusPerustiedotView;
+
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.methods.GetMethod;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -51,6 +52,8 @@ public class TarjontaWebApplication extends TarjontaApplication {
     private String developmentRedirect;
     @Value("${tarjonta-app.dev.theme:}")
     private String developmentTheme;
+    @Value("${tarjonta.public.webservice.url.backend}")
+    private String tarjontaBackendUrl;
     @Autowired
     private TarjontaAdminService tarjontaAdminService;
     @Autowired
@@ -129,7 +132,54 @@ public class TarjontaWebApplication extends TarjontaApplication {
         });
 
         hl.addComponent(btnKomoTest);
+
+
+        
+        final Button btnIndexKoulutukset = new Button("Indeksoi koulutukset", new Button.ClickListener() {
+            private static final long serialVersionUID = 5019806363620874205L;
+
+            @Override
+            public void buttonClick(ClickEvent event) {
+                String urlString = tarjontaBackendUrl.substring(0, tarjontaBackendUrl.indexOf("/services")) + "/rest/indexer/koulutukset?clear=true";
+                try {
+                    LOG.debug("Indeksoi koulutukset: {}", urlString);
+                    
+                    GetMethod get = new GetMethod(urlString);
+                    httpClient.executeMethod(get);
+                    String responseContent = new String(get.getResponseBodyAsString());
+                    LOG.debug("Indeksoi koulutukset done:{}", responseContent);
+                } catch (Throwable ex) {
+                    LOG.error("Failed to index koulutukset", ex);
+                }
+            }
+        });
+
+        hl.addComponent(btnIndexKoulutukset);
+
+        final Button btnHakukohteet = new Button("Indeksoi hakukohteet", new Button.ClickListener() {
+            private static final long serialVersionUID = 5019806363620874205L;
+
+            @Override
+            public void buttonClick(ClickEvent event) {
+                String urlString = tarjontaBackendUrl.substring(0, tarjontaBackendUrl.indexOf("/services")) + "/rest/indexer/hakukohteet?clear=true";
+                try {
+                    LOG.debug("Indeksoi hakukohteet: {}", urlString);
+                    
+                    GetMethod get = new GetMethod(urlString);
+                    httpClient.executeMethod(get);
+                    String responseContent = new String(get.getResponseBodyAsString());
+                    LOG.debug("Indeksoi hakukophteet done:{}", responseContent);
+                } catch (Throwable ex) {
+                    LOG.error("Failed to index hakukohteet", ex);
+                }
+            }
+        });
+
+        hl.addComponent(btnHakukohteet);
+
     }
+
+    private HttpClient httpClient = new HttpClient();
 
     public void toTarjonta() {
         this.removeWindow(window);
