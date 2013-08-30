8ebf60575352d582f0f93c2d2d0b1e0573a71607
jussijartamo <jussi@jartamo@gofore.com>
2013-08-30 15:01:18 +0300

    VT-527 palvelinpuolen filtteröinti

--- tarjonta-service/src/main/java/fi/vm/sade/tarjonta/service/impl/resources/HakuResourceImpl.java
+++ tarjonta-service/src/main/java/fi/vm/sade/tarjonta/service/impl/resources/HakuResourceImpl.java
@@ -1,17 +1,24 @@
 package fi.vm.sade.tarjonta.service.impl.resources;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import javax.annotation.Nullable;
+
+import org.apache.commons.lang.StringUtils;
 import org.apache.cxf.jaxrs.cors.CrossOriginResourceSharing;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.convert.ConversionService;
 import org.springframework.transaction.annotation.Transactional;
+
+import com.google.common.base.Predicate;
+import com.google.common.collect.Collections2;
 
 import fi.vm.sade.organisaatio.api.model.OrganisaatioService;
 import fi.vm.sade.tarjonta.dao.HakuDAO;
@@ -25,7 +32,9 @@ import fi.vm.sade.tarjonta.service.resources.dto.OidRDTO;
 import fi.vm.sade.tarjonta.service.search.HakukohdeListaus;
 import fi.vm.sade.tarjonta.service.search.HakukohteetKysely;
 import fi.vm.sade.tarjonta.service.search.HakukohteetVastaus;
+import fi.vm.sade.tarjonta.service.search.HakukohteetVastaus.HakukohdeTulos;
 import fi.vm.sade.tarjonta.service.search.TarjontaSearchService;
+import fi.vm.sade.tarjonta.service.types.MonikielinenTekstiTyyppi;
 import fi.vm.sade.tarjonta.service.types.TarjontaTila;
 import fi.vm.sade.tarjonta.shared.TarjontaKoodistoHelper;
 
@@ -133,6 +142,8 @@ public class HakuResourceImpl implements HakuResource {
     @Override
     public HakukohdeTulosRDTO getByOIDHakukohdeTulos(String oid, String searchTerms, int count, int startIndex,
             Date lastModifiedBefore, Date lastModifiedSince) {
+        final String filtterointiAvain = "fi"; // TODO: <- rajapintaan
+        final String filtterointiTeksti = StringUtils.capitalize(StringUtils.trimToEmpty(searchTerms));
         LOG.debug("/haku/{}/hakukohdetulos -- getByOIDHakukohdeTulos()", oid);
 
         if (count <= 0) {
@@ -141,18 +152,43 @@ public class HakuResourceImpl implements HakuResource {
         }
         HakukohteetKysely hakukohteetKysely = new HakukohteetKysely();
         hakukohteetKysely.setHakuOid(oid);
-        hakukohteetKysely.setNimi(searchTerms);
-
-        LOG.debug("  kysely for haku '{}' with terms '{}'", new Object[] { oid, searchTerms });
         HakukohteetVastaus v = tarjontaSearchService.haeHakukohteet(hakukohteetKysely);
-        int size = v.getHakukohdeTulos().size();
+        LOG.debug("  kysely for haku '{}' found '{}'", new Object[] { oid, v.getHakukohdeTulos().size() });
+        Collection<HakukohdeTulos> tulokset = v.getHakukohdeTulos();
+        // filtteroi tarvittaessa tulokset joko tarjoaja- tai hakukohdenimen
+        // mukaan!
+        if (!filtterointiTeksti.isEmpty()) {
+            tulokset = Collections2.filter(tulokset, new Predicate<HakukohdeTulos>() {
+                private String haeTekstiAvaimella(MonikielinenTekstiTyyppi tekstit) {
+                    for (MonikielinenTekstiTyyppi.Teksti teksti : tekstit.getTeksti()) {
+                        if (filtterointiAvain.equals(teksti.getKieliKoodi())) {
+                            return StringUtils.capitalize(teksti.getValue());
+                        }
+                    }
+                    LOG.debug("Avain {} doesnt match any languages!", filtterointiAvain);
+                    return StringUtils.EMPTY;
+                }
+
+                public boolean apply(@Nullable HakukohdeTulos tulos) {
+                    String tarjoajaNimi = haeTekstiAvaimella(tulos.getHakukohde().getTarjoaja().getNimi());
+                    String hakukohdeNimi = haeTekstiAvaimella(tulos.getHakukohde().getNimi());
+
+                    return tarjoajaNimi.startsWith(filtterointiTeksti) || hakukohdeNimi.startsWith(filtterointiTeksti);
+                }
+            });
+        }
+        int size = tulokset.size();
+        LOG.debug("  after filtering results found '{}'", size);
         List<HakukohdeNimiRDTO> results = new ArrayList<HakukohdeNimiRDTO>();
-        for (int i = 0; i < count; ++i) {
-            int index = startIndex + i;
-            if (index >= size) {
-                break;
+        int index = 0;
+        for (HakukohdeTulos tulos : tulokset) {
+            if (index < startIndex) {
+                continue; // skip
             }
-            HakukohdeListaus hakukohde = v.getHakukohdeTulos().get(index).getHakukohde();
+            if (index > startIndex + count) {
+                break; // done!
+            }
+            HakukohdeListaus hakukohde = tulos.getHakukohde();
             HakukohdeNimiRDTO rdto = new HakukohdeNimiRDTO();
             // tarvitaanko?
             // result.setHakukohdeNimi(...)
@@ -163,6 +199,7 @@ public class HakuResourceImpl implements HakuResource {
             rdto.setHakukohdeOid(hakukohde.getOid());
             rdto.setHakukohdeTila(hakukohde.getTila() != null ? hakukohde.getTila().name() : null);
             results.add(rdto);
+            ++index;
         }
         return new HakukohdeTulosRDTO(size, results);
     }
@@ -170,8 +207,7 @@ public class HakuResourceImpl implements HakuResource {
     private Map<String, String> convertMonikielinenToMap(
             fi.vm.sade.tarjonta.service.types.MonikielinenTekstiTyyppi monikielinenTekstiTyyppi) {
         Map<String, String> result = new HashMap<String, String>();
-        for (fi.vm.sade.tarjonta.service.types.MonikielinenTekstiTyyppi.Teksti teksti : monikielinenTekstiTyyppi
-                .getTeksti()) {
+        for (MonikielinenTekstiTyyppi.Teksti teksti : monikielinenTekstiTyyppi.getTeksti()) {
             result.put(tarjontaKoodistoHelper.convertKielikoodiToKieliUri(teksti.getKieliKoodi()), teksti.getValue());
         }
         return result;
