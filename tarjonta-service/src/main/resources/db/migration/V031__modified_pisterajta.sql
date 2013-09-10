6cd2c4c6c0274bf3b188c64e4bc8d414c3c23e65
mholi <markus.holi@proactum.fi>
2013-09-10 08:18:47 +0200

    KJOH-590: kantamuutokset

---
+++ tarjonta-service/src/main/resources/db/migration/V031__modified_pisterajta.sql
@@ -0,0 +1,5 @@
+alter table pisteraja alter column alinhyvaksyttypistemaara type decimal(10,2);
+
+alter table pisteraja alter column alinpistemaara type decimal(10,2);
+
+alter table pisteraja alter column ylinpistemaara type decimal(10,2);
