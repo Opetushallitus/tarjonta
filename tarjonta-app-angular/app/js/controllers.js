fa2b2ad246e65430f5938018c1371f444cfcef20
Marko Lyly <Marko.Lyly@proactum.fi>
2013-09-17 14:36:27 +0300

    Testing resource handling.

--- tarjonta-app-angular/app/js/controllers.js
+++ tarjonta-app-angular/app/js/controllers.js
@@ -2,9 +2,25 @@
 
 /* Controllers */
 
-angular.module('tarjontaApp.controllers', []).
-        controller('MyCtrl1', [function() {
+var app = angular.module('tarjontaApp.controllers', []);
+
+app.controller('MyCtrl1', [function() {
         console.log("MyCtrl1");
-    }])
-        .controller('MyCtrl2', [function() {
+    }]);
+
+app.controller('MyCtrl2', ['$scope', 'instagram', function($scope, instagram) {
+
+        console.log("MyCtrl2");
+
+        $scope.foo = "FOO";
+        $scope.pics = [];
+
+        // Use the instagram service and fetch a list of the popular pics
+        instagram.fetchPopular(function(data) {
+
+            // Assigning the pics array will cause the view
+            // to be automatically redrawn by Angular.
+            $scope.pics = data;
+        });
+
     }]);
