dce46ee892ae2fe54ef54b13fc899a9c066994fd
Marko Lyly <Marko.Lyly@proactum.fi>
2013-09-18 08:27:56 +0300

    "The Lost code returns II - revenge of the Controllers"

--- tarjonta-app-angular/app/js/controllers.js
+++ tarjonta-app-angular/app/js/controllers.js
@@ -2,9 +2,22 @@
 
 /* Controllers */
 
-angular.module('tarjontaApp.controllers', []).
-        controller('MyCtrl1', [function() {
-        console.log("MyCtrl1");
-    }])
-        .controller('MyCtrl2', [function() {
+var app = angular.module('tarjontaApp.controllers', ['tarjontaApp.services']);
+
+app.controller('MyCtrl1', [function() {
+        console.log("MyCtrl1()");
     }]);
+
+app.controller('MyCtrl2', function($scope, instagram) {
+    console.log("MyCtrl2()");
+
+    $scope.pics = [];
+    $scope.page = 0;
+    $scope.pageSize = 2;
+
+    instagram.fetchPopular(function(data) {
+        $scope.pics = data;
+    });
+
+
+});
--- tarjonta-app-angular/app/js/services.js
+++ tarjonta-app-angular/app/js/services.js
@@ -5,5 +5,36 @@
 
 // Demonstrate how to register services
 // In this case it is a simple value service.
-angular.module('tarjontaApp.services', []).
-  value('version', '0.1');
+var app = angular.module('tarjontaApp.services', ['ngResource']);
+
+app.value('version', '0.1');
+
+// http://tutorialzine.com/2013/08/learn-angularjs-5-examples/
+app.factory('instagram', function($resource) {
+
+    return {
+        fetchPopular: function(callback) {
+
+            // The ngResource module gives us the $resource service. It makes working with
+            // AJAX easy. Here I am using the client_id of a test app. Replace it with yours.
+
+            var api = $resource('https://api.instagram.com/v1/media/popular?client_id=:client_id&callback=JSON_CALLBACK', {
+                client_id: '642176ece1e7445e99244cec26f4de1f'
+            }, {
+                // This creates an action which we've chosen to name "fetch". It issues
+                // an JSONP request to the URL of the resource. JSONP requires that the
+                // callback=JSON_CALLBACK part is added to the URL.
+
+                fetch: {method: 'JSONP'}
+            });
+
+            api.fetch(function(response) {
+
+                // Call the supplied callback function
+                callback(response.data);
+
+            });
+        }
+    }
+
+});
