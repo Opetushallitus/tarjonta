d580230866b31a1f7ac77301a6bc9b3e36b567fb
Marko Lyly <Marko.Lyly@proactum.fi>
2013-09-19 14:24:00 +0300

    Localisation to separate module. Added initial "auth.js" for authorization.

--- tarjonta-app-angular/app/js/controllers.js
+++ tarjonta-app-angular/app/js/controllers.js
@@ -1,40 +1,5 @@
 angular.module('tarjontaApp.controllers', ['tarjontaApp.services'])
 
-.controller('RootCtrl', function($scope, Localisation) {
-    console.log("RootCtrl()");
-
-    // TODO how to get browser locale ? use locale?
-    $scope.locale = "fi";
-    $scope.localisations = [];
-
-    Localisation.query(function(data) {
-        console.log("Loaded: " + data);
-        $scope.localisations = data;
-    });
-
-    // Returns translation if it exists
-    $scope.t = function(key, params) {
-        console.log("t(" + key + ", " + params + ")");
-        var v = $scope.localisations[key];
-
-        if (v != undefined) {
-            var result = v.value;
-
-            if (params != undefined) {
-                result = result.replace(/{(\d+)}/g, function(match, number) {
-                    return typeof params[number] != 'undefined' ? params[number] : match;
-                });
-            }
-
-            return result;
-        } else {
-            // Unknown translation, maybe create placeholder for it?
-            return "[" + key + "]";
-        }
-    };
-})
-
-
 .controller('MyCtrl1', [function() {
         console.log("MyCtrl1()");
     }])
