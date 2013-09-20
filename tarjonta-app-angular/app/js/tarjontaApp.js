9e0258748c3012a11ad8f4903a3bfe58902a1fc6
tuomaskatva <tuomas.katva@gmail.com>
2013-09-20 16:20:34 +0300

    Added initial koodistoService

--- tarjonta-app-angular/app/js/tarjontaApp.js
+++ tarjonta-app-angular/app/js/tarjontaApp.js
@@ -25,11 +25,13 @@ angular.module('tarjontaApp',
             'ngRoute',
             'ngResource',
             'loading',
-            'localisation'
+            'localisation',
+            'Koodisto'
         ]);
 
 angular.module('tarjontaApp').config(['$routeProvider', function($routeProvider)
     {
+
         $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'})
         .when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'})
 
