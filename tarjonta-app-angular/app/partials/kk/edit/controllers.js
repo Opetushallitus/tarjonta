87a820c70e03d82a75dabb5fee8695fa1a8ca0eb
Marko Lyly <Marko.Lyly@proactum.fi>
2013-09-23 11:41:08 +0300

    routing...

--- tarjonta-app-angular/app/partials/kk/edit/controllers.js
+++ tarjonta-app-angular/app/partials/kk/edit/controllers.js
@@ -2,9 +2,9 @@
 
 /* Controllers */
 
-var app = angular.module('app.kk.edit.ctrl', []);
+var app_kk_edit_ctrl = angular.module('app.kk.edit.ctrl', []);
 
-app.controller('EditController', ['$scope', 'TarjontaService',
+app_kk_edit_ctrl.controller('KKEditController', ['$scope', 'TarjontaService',
     function FormTutkintoController($scope, tarjontaService) {
         $scope.searchByOid = "1.2.246.562.5.2013091114080489552096";
         $scope.opetuskieli = 'kieli_fi';
