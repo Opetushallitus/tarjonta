790b7d6ff83373a669f3ea7546fd9b9920151c8f
Sami Siren <sami.siren@proactum.fi>
2013-09-24 15:28:50 +0300

    KJOH-606 jotain kursittu kasaan

--- tarjonta-app-angular/app/partials/tarjontaApp.js
+++ tarjonta-app-angular/app/partials/tarjontaApp.js
@@ -31,7 +31,9 @@ angular.module('app',
             'localisation',
             'Koodisto',
             'KoodistoCombo',
-            'zippy'
+            'zippy',
+            'app.organisaatiohaku',
+            'angularTreeview'
         ]);
 
 
@@ -39,7 +41,9 @@ angular.module('app').config(['$routeProvider', function($routeProvider)
     {
 
         $routeProvider
-                .when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'})
+        		.when('/etusivu', {templateUrl: 'partials/etusivu.html'})
+                
+        		.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'})
 
                 .when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'})
 
@@ -77,7 +81,8 @@ angular.module('app').config(['$routeProvider', function($routeProvider)
 
                 .when('/kk/tutkintoOhjelma', {templateUrl: 'partials/kk/edit/selectTutkintoOhjelmaOpener.html'})
 
-                .otherwise({redirectTo: '/view1'});
+        		
+                .otherwise({redirectTo: '/etusivu'});
 
     }]);
 
