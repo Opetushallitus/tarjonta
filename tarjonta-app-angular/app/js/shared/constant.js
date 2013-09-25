7f569b45731867b2635a8035eb73c50a60845b3b
timosan <timo.santasalo@proactum.fi>
2013-09-25 16:55:44 +0300

    merge

---
+++ tarjonta-app-angular/app/js/shared/constant.js
@@ -0,0 +1,49 @@
+// ROOT organisation
+var OPH_ORG_OID = "1.2.246.562.10.00000000001";
+
+//All application directory names:
+var KK_TUTKINTO = 'kkTutkinto';
+var MAIN = 'tarjonta';
+
+// Tarjonnan tilat
+// TODO Generoi automaattisesti enumin fi.vm.sade.tarjonta.shared.types.TarjontaTila mukaan
+var TARJONTA_TILAT = [ "LUONNOS", "VALMIS", "JULKAISTU", "PERUTTU", "KOPIOITU" ];
+
+//Angular modules:
+var NG_ROUTE = 'ngRoute';
+var NG_RESOURCE = 'ngResource';
+
+//All sub module file names:
+var SERVICES = '.services';
+var FILTERS = '.filters';
+var DIRECTIVES = '.directives';
+var CTRL = '.controllers';
+
+var APP_PREFIX = 'App';
+
+//All available services:
+var TARJONTA_SERVICE = MAIN + APP_PREFIX + SERVICES;
+
+function toApp(appNameWithoutPrefix) {
+    return appNameWithoutPrefix + APP_PREFIX;
+}
+
+function toSub(appNameWithoutPrefix, subModule) {
+    return appNameWithoutPrefix + APP_PREFIX + subModule;
+}
+
+function toDependency(appName, arr) {
+    //add module base files
+    arr.push(
+            appName + APP_PREFIX + CTRL,
+            appName + APP_PREFIX + DIRECTIVES,
+            appName + APP_PREFIX + FILTERS,
+            appName + APP_PREFIX + SERVICES);
+    return arr;
+}
+
+function toTemplateUrl(moduleName, fileName) {
+    //add module base files
+    return 'partials/' + moduleName + '/' + fileName + '.html';
+}
+
