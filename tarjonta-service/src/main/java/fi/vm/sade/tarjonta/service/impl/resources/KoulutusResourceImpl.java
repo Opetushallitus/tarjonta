af2916a31c2c788016562ce085a67cc5bee6110c
timosan <timo.santasalo@proactum.fi>
2013-10-09 13:53:47 +0300

    tarjonta-rest-api: tilasiirtymä palauttavat stringin (cxf:n bugin kierto)

--- tarjonta-service/src/main/java/fi/vm/sade/tarjonta/service/impl/resources/KoulutusResourceImpl.java
+++ tarjonta-service/src/main/java/fi/vm/sade/tarjonta/service/impl/resources/KoulutusResourceImpl.java
@@ -223,14 +223,14 @@ public class KoulutusResourceImpl implements KoulutusResource {
     }
     
     @Override
-    public TarjontaTila updateTila(String oid, TarjontaTila tila) {
+    public String updateTila(String oid, TarjontaTila tila) {
     	KoulutusmoduuliToteutus komoto = koulutusmoduuliToteutusDAO.findByOid(oid);
     	Preconditions.checkArgument(komoto!=null, "Koulutusta ei löytynyt: %s", oid);
     	if (!komoto.getTila().acceptsTransitionTo(tila)) {
-    		return komoto.getTila();
+    		return komoto.getTila().toString();
     	}
     	komoto.setTila(tila);
     	koulutusmoduuliToteutusDAO.update(komoto);
-    	return tila;
+    	return tila.toString();
     }
 }
