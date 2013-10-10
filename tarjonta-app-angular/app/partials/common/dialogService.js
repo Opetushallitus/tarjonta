d5fae5bc2db86bfee690c8c484f8fb5b4393d7de
Marko Lyly <Marko.Lyly@proactum.fi>
2013-10-10 16:47:55 +0300

    KJOH-614, KJOH-615, KJOH-616 Documentation added.

--- tarjonta-app-angular/app/partials/common/dialogService.js
+++ tarjonta-app-angular/app/partials/common/dialogService.js
@@ -1,23 +1,84 @@
 
-var app = angular.module('app.dialog', ['ui.bootstrap', 'ngAnimate']);
+var app = angular.module('app.dialog', ['ui.bootstrap', 'ngAnimate', 'ngSanitize']);
 
+/**
+ * Very simplistinc dialog service. Simple yes now question at the moment.
+ *
+ * (Any more interaction in the UI, just use the $modal then)
+ *
+ * Usage example:
+ * <pre>
+ *       <div class="btn" ng-click="goBackCustom()">Back?</div>
+ *
+ *       $scope.goBackSimple = function() {
+ *           var d = dialogService.showDialog();
+ *           d.result.then(function(data) {
+ *               if ("ACTION" === data) {
+ *                   window.history.back();
+ *               }
+ *           });
+ *       };
+ *
+ *       $scope.goBackCustom = function() {
+ *           var texts = {title: "Really?", description: "<u>Really</u> go <b>back</b>?}", ok: "BACK!!!", cancel: "Nope..."};
+ *           var opts = {backdrop: 'static', // true, false, 'static'
+ *                       keyboard: false     // close with ESC?
+ *           };
+ *
+ *           var d = dialogService.showDialog(texts, opts);
+ *           d.result.then(function(data) {
+ *               $log.info("GOT: ", data); // results: "ACTION" or "CANCEL"
+ *               if ("ACTION" === data) {
+ *                   window.history.back();
+ *               }
+ *           });
+ *       };
+ *
+ * <pre>
+ *
+ * @param {type} param1
+ * @param {type} param2
+ */
 app.service('dialogService', ['$modal', '$log', '$rootScope',
     function($modal, $log, $rootScope) {
 
         var dialogDefaults = {
             templateUrl: 'partials/common/dialog.html',
             controller: "DialogServiceController",
-            backdrop: true,
-            keyboard: true
+            backdrop: true, // true, "static", false
+            keyboard: true  // closable with ESC?
         };
 
         var dialogTextDefaults = {
             title: "Really?",
-            description: "Really do <b>that</b>?",
-            okText: "YEAS please!",
-            closeText: "Naaaah..."
+            description: "<i>Really</i> do <b>that</b>?",
+            ok: "YEAS please!",
+            cancel: "Naaaah..."
         };
 
+        /**
+         * Returns ($modal) dialog instance which can then be used to wait for user interaction: dialogInstance.result.then(...
+         *
+         * customDialogTextDefaults:
+         * <ul>
+         *   <li>title: "foobar"</li>
+         *   <li>description: "foobar <b>text</b>"</li>
+         *   <li>ok: "Proceed"</li>
+         *   <li>cancel: "abort"</li>
+         * </ul>
+         *
+         * customDialogDefaults:
+         * <ul>
+         *   <li>backdrop: true, 'static', false</li>
+         *   <li>keyboard: true / false</li>
+         *   <li>templateUrl: 'MyController'</li>
+         *   <li>templateUrl: 'MyTemplate.html'</li>
+         * </ul>
+         *
+         * @param Object customDialogTextDefaults
+         * @param {type} customDialogDefaults
+         * @returns {@exp;$modal@call;open}
+         */
         this.showDialog = function(customDialogTextDefaults, customDialogDefaults) {
             var tempDialogDefaults = {};
             angular.extend(tempDialogDefaults, dialogDefaults, customDialogDefaults);
@@ -40,6 +101,13 @@ app.service('dialogService', ['$modal', '$log', '$rootScope',
 
     }]);
 
+/**
+ * Simple default controller for the "partials/common/dialog.html" template.
+ * Closes dialog with result of "ACTION" or "CANCEL".
+ *
+ * @param {type} param1
+ * @param {type} param2
+ */
 app.controller('DialogServiceController', ['$scope', '$log', '$modalInstance',
     function($scope, $log, $modalInstance) {
 
