e3a5e9c313a17af577e56b406408b601c6e8079c
mholi <markus.holi@proactum.fi>
2013-10-16 19:04:51 +0200

    KJOH-629: flyway korjaus

--- tarjonta-service/src/main/resources/db/migration/V038__add_laajuusfields_to_komoto.sql
+++ tarjonta-service/src/main/resources/db/migration/V038__add_laajuusfields_to_komoto.sql
@@ -1,4 +1,4 @@
 ALTER TABLE koulutusmoduuli_toteutus DROP COLUMN IF EXISTS opintojen_laajuus_arvo;
-ALTER TABLE koulutusmoduuli_toteutus DROP COLUMN IF EXISTS opintojen_laajuus_yksikko IF EXISTS;
+ALTER TABLE koulutusmoduuli_toteutus DROP COLUMN IF EXISTS opintojen_laajuus_yksikko;
 ALTER TABLE koulutusmoduuli_toteutus ADD COLUMN opintojen_laajuus_arvo varchar(255);
 ALTER TABLE koulutusmoduuli_toteutus ADD COLUMN opintojen_laajuus_yksikko varchar(255);
