81d3a4f6be1bc89bb1626537416da94ebb13ed49
mlyly <Marko.Lyly@proactum.fi>
2013-11-13 15:34:19 +0200

    OVT-6157 fixed the tests I broke - sorry.

--- tarjonta-app-angular/test/unit/shared/localisationSpec.js
+++ tarjonta-app-angular/test/unit/shared/localisationSpec.js
@@ -26,7 +26,7 @@ describe('localisation', function() {
             "key-env-2": "mock-value-env-2",
             "tarjonta.localisations": [{ key: "this.is.a.test.too_XXX", value: THE_OFFICIAL_TEST_VALUE_FI, locale: "fi"}],
             "casUrl" : "cas_myroles_tiimi2",
-            "cas.myroles" : 
+            "cas.myroles" :
             	["USER_tiimi2", "APP_ANOMUSTENHALLINTA", "APP_ANOMUSTENHALLINTA_CRUD", "APP_ORGANISAATIOHALLINTA",
             	    "APP_ORGANISAATIOHALLINTA_CRUD", "APP_HENKILONHALLINTA", "APP_HENKILONHALLINTA_CRUD", "APP_KOODISTO",
             	    "APP_KOODISTO_CRUD", "APP_KOOSTEROOLIENHALLINTA", "APP_KOOSTEROOLIENHALLINTA_CRUD", "APP_OID",
@@ -66,10 +66,10 @@ describe('localisation', function() {
         // When an unknown translation is referred to - a "["+key+"]" value should be returned
         it('unknown keys should be wrapped to []', inject(function() {
             var key1 = "this.is.a.test";
-            expect(scope.t(key1)).toEqual("[" + key1.replace(/\./g, " . ") + "]");
+            expect(scope.t(key1)).toEqual("[" + key1 + "-fi]");
 
             var key2 = "this.is.a.test.too";
-            expect(scope.t(key2)).toEqual("[" + key2.replace(/\./g, " . ") + "]");
+            expect(scope.t(key2)).toEqual("[" + key2 + "-fi]");
 
             var key3 = "this.is.a.test.too_XXX";
             expect(scope.t(key3)).toEqual(THE_OFFICIAL_TEST_VALUE_FI);
