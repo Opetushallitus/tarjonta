e62fd6e179d5a10743bd48481dc61d17d3119d62
mlyly <Marko.Lyly@proactum.fi>
2013-12-10 14:35:06 +0200

    Logging with location information.

--- tarjonta-service/src/main/java/fi/vm/sade/tarjonta/service/AllowAllCorsRequestsFilter.java
+++ tarjonta-service/src/main/java/fi/vm/sade/tarjonta/service/AllowAllCorsRequestsFilter.java
@@ -12,13 +12,60 @@
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  */
-
 package fi.vm.sade.tarjonta.service;
 
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
+ * Simple filter to ALLOW ALL CORS-requests. At development time it is _really_ annoying to have CORS-related difficulties.
  *
  * @author mlyly
  */
-public class AllowAllCorsRequestsFilter {
+public class AllowAllCorsRequestsFilter implements Filter {
+
+    private static final Logger LOG = LoggerFactory.getLogger(AllowAllCorsRequestsFilter.class);
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        LOG.info("init()");
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        LOG.info("doFilter()");
+
+        if (response instanceof HttpServletResponse) {
+            HttpServletRequest req = (HttpServletRequest) request;
+            String headerOrigin = req.getHeader("Origin");
+
+            if (headerOrigin != null) {
+                LOG.info("  fixing CORS --> allow: '{}'", headerOrigin);
+
+                HttpServletResponse res = (HttpServletResponse) response;
+                res.addHeader("Access-Control-Allow-Origin", headerOrigin);
+                res.addHeader("Access-Control-Allow-Credentials", "true");
+                res.addHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE, HEAD");
+                res.addHeader("Access-Control-Allow-Headers", "X-PINGOTHER, Origin, X-Requested-With, Content-Type, Accept");
+                res.addHeader("Access-Control-Max-Age", "1728000");
+            }
+        }
+
+        chain.doFilter(request, response);
+    }
+
+    @Override
+    public void destroy() {
+        LOG.info("desctroy()");
+    }
 
 }
