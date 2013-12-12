1d0385ab91f5c2caad863dd0b6bacf181ae0ed57
Sami Siren <sami.siren@proactum.fi>
2013-12-12 14:09:00 +0200

    OVT-5946 palauta muuttuneet komoto/hakukohde oidit palvelusta

---
+++ tarjonta-service/src/main/java/fi/vm/sade/tarjonta/service/impl/Tilamuutokset.java
@@ -0,0 +1,27 @@
+package fi.vm.sade.tarjonta.service.impl;
+
+import java.util.TreeSet;
+
+public class Tilamuutokset {
+
+    private TreeSet<String> muutetutHakukohteet = new TreeSet<String>();
+
+    private TreeSet<String> muutetutKomotot = new TreeSet<String>();
+
+    public TreeSet<String> getMuutetutHakukohteet() {
+        return muutetutHakukohteet;
+    }
+
+    public void setMuutetutHakukohteet(TreeSet<String> muutetutHakukohteet) {
+        this.muutetutHakukohteet = muutetutHakukohteet;
+    }
+
+    public TreeSet<String> getMuutetutKomotot() {
+        return muutetutKomotot;
+    }
+
+    public void setMuutetutKomotot(TreeSet<String> muutetutKomotot) {
+        this.muutetutKomotot = muutetutKomotot;
+    }
+
+}
