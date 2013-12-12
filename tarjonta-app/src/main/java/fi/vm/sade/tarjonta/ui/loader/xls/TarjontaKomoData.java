05785d222d8c8dc9c71ba431408f13531bbb8d17
Jani Wilen <jani.wilen@proactum.fi>
2013-12-12 13:56:26 +0200

    OVT-6412, lisätty app-palveluun puuttuva lukiolinja

--- tarjonta-app/src/main/java/fi/vm/sade/tarjonta/ui/loader/xls/TarjontaKomoData.java
+++ tarjonta-app/src/main/java/fi/vm/sade/tarjonta/ui/loader/xls/TarjontaKomoData.java
@@ -30,6 +30,7 @@ import fi.vm.sade.tarjonta.service.TarjontaPublicService;
 import fi.vm.sade.tarjonta.service.types.GeneerinenTilaTyyppi;
 import fi.vm.sade.tarjonta.service.types.HaeKoulutusmoduulitKyselyTyyppi;
 import fi.vm.sade.tarjonta.service.types.HaeKoulutusmoduulitVastausTyyppi;
+import fi.vm.sade.tarjonta.service.types.KoulutusasteTyyppi;
 import static fi.vm.sade.tarjonta.service.types.KoulutusasteTyyppi.AMMATILLINEN_PERUSKOULUTUS;
 import static fi.vm.sade.tarjonta.service.types.KoulutusasteTyyppi.LUKIOKOULUTUS;
 import static fi.vm.sade.tarjonta.service.types.KoulutusasteTyyppi.VALMENTAVA_JA_KUNTOUTTAVA_OPETUS;
@@ -50,6 +51,7 @@ import fi.vm.sade.tarjonta.ui.helper.conversion.ConversionUtils;
 import fi.vm.sade.tarjonta.ui.helper.conversion.SearchWordUtil;
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
@@ -444,6 +446,9 @@ public class TarjontaKomoData {
                             if (dbParentOid == null) {
                                 dbParentOid = parentKomosReadyForInsertAndUpdate.get(koulutuskoodiUri).getOid();
                             }
+
+                            log.error("{} {}", dbParentOid, koulutuskoodiUri + "/" + koulutusohjelmakoodiUri + "/" + lukiolinjaUri);
+
                             Preconditions.checkNotNull(dbParentOid, "Parent OID cannot be null.");
                             //update childrend
                             switch (excelChildKomo.getKoulutustyyppi()) {
@@ -459,12 +464,14 @@ public class TarjontaKomoData {
                                         updateRelations(excelChildKomo, dbChildKomos.get(koulutusohjelmakoodiUri).getOid(), dbParentOid, koulutuskoodiUri + "/" + koulutusohjelmakoodiUri);
                                         tarjontaAdminService.paivitaKoulutusmoduuli(excelChildKomo);
                                     } else if (dbParentOid != null) {
+                                        log.error("{} {}", koulutusohjelmakoodiUri, dbChildKomos.containsKey(koulutusohjelmakoodiUri));
+
                                         updateRelations(excelChildKomo, excelChildKomo.getOid(), dbParentOid, koulutuskoodiUri + "/" + koulutusohjelmakoodiUri);
                                         tarjontaAdminService.lisaaKoulutusmoduuli(excelChildKomo);
                                         newKomoOids.add(excelChildKomo.getOid());
                                     }
                                     break;
-                               
+
                                 case LUKIOKOULUTUS:
                                     //log.debug("LUKIOKOULUTUS {}", lukiolinjaUri);
 
@@ -522,9 +529,30 @@ public class TarjontaKomoData {
     private void separateDbKomos() {
         int count = 1;
 
-        HaeKoulutusmoduulitVastausTyyppi allKomos = tarjontaPublicService.haeKoulutusmoduulit(new HaeKoulutusmoduulitKyselyTyyppi());
-        if (allKomos != null && !allKomos.getKoulutusmoduuliTulos().isEmpty()) {
-            List<KoulutusmoduuliTulos> allModules = allKomos.getKoulutusmoduuliTulos();
+        HaeKoulutusmoduulitKyselyTyyppi hae = new HaeKoulutusmoduulitKyselyTyyppi();
+        HaeKoulutusmoduulitVastausTyyppi allKomos = tarjontaPublicService.haeKoulutusmoduulit(hae);
+        List<KoulutusmoduuliTulos> allModules = Lists.newArrayList();
+
+        final Set<KoulutusasteTyyppi> allowedTypes = new HashSet<KoulutusasteTyyppi>();
+        allowedTypes.add(KoulutusasteTyyppi.AMMATILLINEN_PERUSKOULUTUS);
+        allowedTypes.add(KoulutusasteTyyppi.AMM_OHJAAVA_JA_VALMISTAVA_KOULUTUS);
+        allowedTypes.add(KoulutusasteTyyppi.LUKIOKOULUTUS);
+        allowedTypes.add(KoulutusasteTyyppi.MAAHANM_AMM_VALMISTAVA_KOULUTUS);
+        allowedTypes.add(KoulutusasteTyyppi.MAAHANM_LUKIO_VALMISTAVA_KOULUTUS);
+        allowedTypes.add(KoulutusasteTyyppi.PERUSOPETUKSEN_LISAOPETUS);
+        allowedTypes.add(KoulutusasteTyyppi.VALMENTAVA_JA_KUNTOUTTAVA_OPETUS);
+        allowedTypes.add(KoulutusasteTyyppi.VAPAAN_SIVISTYSTYON_KOULUTUS);
+
+        //filter all korkeakoulu komos
+        for (KoulutusmoduuliTulos tulos : allKomos.getKoulutusmoduuliTulos()) {
+            
+            if (allowedTypes.contains(tulos.getKoulutusmoduuli().getKoulutustyyppi())) {
+               log.error("found {}", tulos.getKoulutusmoduuli().getKoulutusohjelmakoodiUri());
+                allModules.addAll(allKomos.getKoulutusmoduuliTulos());
+            }
+        }
+
+        if (allModules != null && !allModules.isEmpty()) {
             for (KoulutusmoduuliTulos t : allModules) {
                 if (count % 10 == 1) {
                     log.info("Processing DB KOMOs... {}", count);
