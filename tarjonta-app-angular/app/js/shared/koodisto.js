24e6f55e99462bab36b1c51161ffdcfad8d6993d
Sami Siren <sami.siren@proactum.fi>
2014-02-11 16:13:35 +0200

    poista logitusta

--- tarjonta-app-angular/app/js/shared/koodisto.js
+++ tarjonta-app-angular/app/js/shared/koodisto.js
@@ -231,7 +231,6 @@ app.factory('Koodisto', function($resource, $log, $q, Config) {
                 var metadata = metadatas[j];
                 nimi[metadata.kieli]=metadata.nimi;
               }
-              console.log("nimi:", nimi);
               ret.resolve(nimi[locale.toUpperCase()]||nimi.FI||nimi.EN||nimi.SV); //fallback
               
             }
