b85c987c349184e5f149e28256ec12694d9ade23
Sami Siren <sami.siren@proactum.fi>
2014-02-28 13:26:09 +0200

    helpota ongelmanselvittelyä ja proxyta liikenne noden kautta haluttuun ympäristöön

--- tarjonta-app-angular/scripts/web-server.js
+++ tarjonta-app-angular/scripts/web-server.js
@@ -1,5 +1,30 @@
 var express = require('express');
 var server = express(); 
+var request = require('request');
+
+var luokka="https://itest-virkailija.oph.ware.fi/"
+var reppu="https://test-virkailija.oph.ware.fi/"
+
+var USE_ENV=reppu;
+
+
+
+var env={
+	remote:USE_ENV
+}
+
+function proxy(server, prefix) {
+  var h=function(req,res){
+	url = env.remote + prefix + req.url;
+	req.pipe(request(url)).pipe(res);
+  }
+	server.use(prefix, h);
+}
+
+proxy(server, "/virkailija-raamit");
+proxy(server, "/cas");
+proxy(server, "/lokalisointi");
+
 server.configure(function(){
   var dir = __dirname + "/../app/";
   console.log("serving directory: ",dir);
