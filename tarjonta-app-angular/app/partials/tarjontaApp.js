da1f671d833947fedacf4b05501b33343cdecd54
mlyly <Marko.Lyly@proactum.fi>
2014-03-26 13:27:08 +0200

    Logging, added more "context" information.

--- tarjonta-app-angular/app/partials/tarjontaApp.js
+++ tarjonta-app-angular/app/partials/tarjontaApp.js
@@ -92,6 +92,113 @@ angular.module('app',
 
 angular.module('app').value("globalConfig", window.CONFIG);
 
+angular.module('app').config(["$provide", function($provide) {
+
+        console.log("tarjontaApp.config - enhance logging...");
+
+        function formatDate(date) {
+            var result = "";
+            if (date) {
+                var d = date; // new Date();
+
+                result = result + ((d.getHours() < 10) ? "0" : "") + d.getHours();
+                result = result + ((d.getMinutes() < 10) ? "0" : "") + d.getMinutes();
+                result = result + ((d.getSeconds() < 10) ? "0" : "") + d.getSeconds();
+            }
+
+            return result;
+        }
+
+
+        $provide.decorator('$log', ["$delegate", function($delegate)
+            {
+                var _$log = (function($log)
+                {
+                    return {
+                        log: $log.log,
+                        info: $log.info,
+                        warn: $log.warn,
+                        debug: $log.debug,
+                        error: $log.error
+                    };
+                })($delegate);
+
+                var prepareLogFn = function(logFn, logLevel, logClass) {
+                    var enhanced = function() {
+                        var args = [].slice.call(arguments),
+                                now = new Date();
+
+                        // Prepend timestamp, level, class + actual result
+                        args[0] = formatDate(now) + " - " + logLevel + " - " + logClass + " :: " + args[0];
+
+                        // Call the original with the output prepended with formatted timestamp
+                        logFn.apply(null, args);
+                    };
+
+                    return enhanced;
+                };
+
+                // Default implementations, no class name
+                $delegate.log = prepareLogFn(_$log.log, "?", "?");
+                $delegate.info = prepareLogFn(_$log.info, "I", "?");
+                $delegate.warn = prepareLogFn(_$log.warn, "W", "?");
+                $delegate.debug = prepareLogFn(_$log.debug, "D", "?");
+                $delegate.error = prepareLogFn(_$log.error, "E", "?");
+
+                // Class spesific implementations
+                $delegate.getInstance = function(logClass) {
+                    return {
+                        log : prepareLogFn(_$log.log, "?", logClass),
+                        info : prepareLogFn(_$log.info, "I", logClass),
+                        warn : prepareLogFn(_$log.warn, "W", logClass),
+                        debug : prepareLogFn(_$log.debug, "D", logClass),
+                        error : prepareLogFn(_$log.error, "E", logClass)
+                    }
+                };
+
+
+//
+//                // Save the original $log.debug()
+//                var debugFn = $delegate.debug;
+//                var infoFn = $delegate.info;
+//
+//                $delegate.debug = function( )
+//                {
+//                    // console.log("log.debug.decorator!");
+//
+//                    var args = [].slice.call(arguments),
+//                            now = new Date();
+//
+//                    // Prepend timestamp
+//                    args[0] = formatDate(now) + " - D - " + args[0];
+//
+//                    // Call the original with the output prepended with formatted timestamp
+//                    debugFn.apply(null, args)
+//                };
+//
+//                $delegate.info = function( )
+//                {
+//                    // console.log("log.info.decorator!");
+//
+//                    var args = [].slice.call(arguments),
+//                            now = new Date();
+//
+//                    // Prepend timestamp
+//                    args[0] = formatDate(now) + " - I - " + args[0];
+//
+//                    // Call the original with the output prepended with formatted timestamp
+//                    infoFn.apply(null, args)
+//                };
+
+
+                return $delegate;
+            }]);
+
+        console.log("tarjontaApp.config - enhance logging... done.");
+
+}]);
+
+
 angular.module('app').config(['$routeProvider', function($routeProvider) {
 
         $routeProvider
