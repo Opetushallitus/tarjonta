d11ed262633be3aac77fff1d6c37f77d8f154ea8
Tuomas Katva <tuomas.katva@gmail.com>
2014-06-17 13:02:50 +0300

    KJOH-808

---
+++ tarjonta-app-angular/app/partials/hakukohde/edit/aiku/naytto/HakukohdeAikuNayttoController.js
@@ -0,0 +1,67 @@
+/**
+ * Created by tuomas on 17.6.2014.
+ */
+
+var app = angular.module('app.kk.edit.hakukohde.ctrl');
+app.controller('HakukohdeAikuNayttoEditController',
+    function($scope,
+             $q,
+             $log,
+             LocalisationService,
+             OrganisaatioService,
+             Koodisto,
+             Hakukohde,
+             AuthService,
+             HakuService,
+             $route ,
+             $modal ,
+             Config,
+             $location,
+             $timeout,
+             TarjontaService,
+             Kuvaus,
+             CommonUtilService,
+             PermissionService) {
+
+
+        $log = $log.getInstance("HakukohdeAikuNayttoEditController");
+
+
+        //TODO: Add naytto specific haku filtering logic
+        var filterHakus = function(hakus) {
+
+            return hakus;
+        };
+
+        /**
+
+         Controller initialization function which is called when controller loads
+
+         */
+        var init = function() {
+
+            $scope.model.userLang  =  AuthService.getLanguage();
+
+            if ($scope.model.userLang === undefined) {
+                $scope.model.userLang = "FI";
+            }
+            $scope.loadKoulutukses(filterHakus);
+            $scope.haeTarjoajaOppilaitosTyypit();
+            $scope.model.continueToReviewEnabled = $scope.checkJatkaBtn($scope.model.hakukohde);
+            $scope.checkIsCopy();
+
+            /*$scope.updateTilaModel($scope.model.hakukohde);*/
+
+            if ($scope.model.hakukohde.hakukelpoisuusVaatimusKuvaukset === undefined) {
+                $scope.model.hakukohde.hakukelpoisuusVaatimusKuvaukset = {};
+            }
+
+            if ($scope.model.hakukohde.kaytetaanJarjestelmanValintaPalvelua === undefined) {
+                $scope.model.hakukohde.kaytetaanJarjestelmanValintaPalvelua = true;
+            }
+
+            $scope.enableOrDisableTabs();
+
+        };
+
+    });
