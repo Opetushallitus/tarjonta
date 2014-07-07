54f790b8f1511c87cf00bc692f7af3659388f298
Sami Siren <sami.siren@proactum.fi>
2014-07-07 11:31:48 +0300

    palauta itest urli

--- tarjonta-app-angular/app/ext/js/env-configuration.js
+++ tarjonta-app-angular/app/ext/js/env-configuration.js
@@ -103,11 +103,11 @@ window.CONFIG = {
         // (also check the tarjonta-rest.properties so that the new property will be included to the Angular APP...)
         //
 
-        //"tarjontaRestUrlPrefix": "https://itest-virkailija.oph.ware.fi/tarjonta-service/rest/v1/",
+        "tarjontaRestUrlPrefix": "https://itest-virkailija.oph.ware.fi/tarjonta-service/rest/v1/",
 
         // "tarjontaRestUrlPrefix": "http://luokka.hard.ware.fi:8302/tarjonta-service/rest/v1/",
         // "tarjontaRestUrlPrefix": "http://luokka.hard.ware.fi:8302/tarjonta-service/rest/v1/",
-        "tarjontaRestUrlPrefix": "http://localhost:8585/tarjonta-service/rest/v1/",
+        //"tarjontaRestUrlPrefix": "http://localhost:8585/tarjonta-service/rest/v1/",
         //"tarjontaRestUrlPrefix" : "http://localhost:8084/tarjonta-service/rest/v1/",
          //"tarjontaRestUrlPrefix": "http://localhost:9090/tarjonta-service/rest/v1/",
 
