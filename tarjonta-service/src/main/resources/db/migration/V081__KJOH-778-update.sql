2db465eafd6f58b56e83046e11c79022d69fefdc
Alexis Troberg <alexis.troberg@gofore.com>
2014-10-15 19:08:54 +0300

    Uusi versio Flyway migraatiolle ja unique key -> primary key

--- tarjonta-service/src/main/resources/db/migration/V081__KJOH-778-update.sql
+++ tarjonta-service/src/main/resources/db/migration/V081__KJOH-778-update.sql
@@ -1,6 +1,6 @@
 -- Lisää unique constraint
 ALTER TABLE "koulutusmoduuli_toteutus_owners"
-ADD CONSTRAINT "koulutusmoduuli_toteutus_owners_koulutusmoduuli_toteutus_id_owneroid_ownertype" UNIQUE ("koulutusmoduuli_toteutus_id", "owneroid", "ownertype");
+ADD CONSTRAINT "koulutusmoduuli_toteutus_owners_koulutusmoduuli_toteutus_id_owneroid_ownertype" PRIMARY KEY ("koulutusmoduuli_toteutus_id", "owneroid", "ownertype");
 
 -- Kopioi tarjoaja-tieto "koulutusmoduuli_toteutus" taulusta uuteen "koulutusmoduuli_toteutus_owners" mappaus tauluun
 INSERT INTO "koulutusmoduuli_toteutus_owners" ("koulutusmoduuli_toteutus_id", "owneroid", "ownertype")
