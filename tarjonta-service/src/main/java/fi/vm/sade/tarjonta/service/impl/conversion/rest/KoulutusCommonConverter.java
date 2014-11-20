f8f78eccfc510df65593fe79c5a530f56f2bc5f0
Alexis Troberg <alexis.troberg@gofore.com>
2014-11-20 19:03:09 +0200

    Fix OVT-8981 väärä virheilmoitus, kun koulutuksella eri kausien alkamispvm:t
    KOULUTUS_ALKAMISPVM_VUOSI_INVALID on käännösavain, jota käytetään aina jos
    koulutuksella on useita alkamispäivämääriä ja nämä kaikki eivät ole samalla
    kaudella.
    
    Aiemmin on ilmeisesti ajateltu, että olisi kaksi eri virheilmoitusta
    (KOULUTUS_ALKAMISPVM_KAUSI_INVALID ja KOULUTUS_ALKAMISPVM_VUOSI_INVALID), mutta
    lokalisaatiopalvelussa on määritelty seuraava käännös KOULUTUS_ALKAMISPVM_VUOSI_INVALID
    avaimelle ja sitä käytetään aina tässä virhetilanteessa
    "Koulutuksen alkamispäivämäärien tulee olla samalla kaudella."

--- tarjonta-service/src/main/java/fi/vm/sade/tarjonta/service/impl/conversion/rest/KoulutusCommonConverter.java
+++ tarjonta-service/src/main/java/fi/vm/sade/tarjonta/service/impl/conversion/rest/KoulutusCommonConverter.java
@@ -424,11 +424,8 @@ public class KoulutusCommonConverter {
 
         //pre-check if the dates are within same date range of kausi + vuosi
         for (Date pvm : dates) {
-            if (!baseKausi.equals(IndexDataUtils.parseKausiKoodi(pvm))) {
-                return KoulutusValidationMessages.KOULUTUS_ALKAMISPVM_KAUSI_INVALID;
-            }
-
-            if (!baseVuosi.equals(IndexDataUtils.parseYearInt(pvm))) {
+            if (!baseKausi.equals(IndexDataUtils.parseKausiKoodi(pvm))
+                || !baseVuosi.equals(IndexDataUtils.parseYearInt(pvm))) {
                 return KoulutusValidationMessages.KOULUTUS_ALKAMISPVM_VUOSI_INVALID;
             }
 
