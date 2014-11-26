c7d45c500313d4266810acef94743a701922c55e
tkarttu <tapani.karttunen@cybercom.com>
2014-11-26 13:54:22 +0200

    OVT-8750 lisäyksiä alkamispäivärajoitteiden asetukseen tallennuksen yhteydessä

--- tarjonta-app-angular/app/partials/koulutus/edit/editController.js
+++ tarjonta-app-angular/app/partials/koulutus/edit/editController.js
@@ -308,6 +308,8 @@ app.controller('BaseEditController', [
                 fnCustomCallbackAfterSave,
                 converter.saveModelConverter(apiModel, $scope.uiModel, tyyppi)
                 );
+
+            $scope.commonLoadModelHandler(form, $scope.model, $scope.uiModel, tyyppi);
         };
 
         $scope.saveByStatusAndApiObject = function(form, tyyppi, fnCustomCallbackAfterSave, apiModelReadyForSave) {
@@ -683,9 +685,11 @@ app.controller('BaseEditController', [
     				if (alkamisPvm.getMonth() < 7) {
     					minY.setFullYear(vuosi, 0, 1);
     					maxY.setFullYear(vuosi, 6, 31);
+    					model.koulutuksenAlkamiskausi.uri = "kausi_k";
 					} else {
     					minY.setFullYear(vuosi, 7, 1);
     					maxY.setFullYear(vuosi, 11, 31);
+    					model.koulutuksenAlkamiskausi.uri = "kausi_s";
 					}
 					$scope.min = minY;
 					$scope.max = maxY;
@@ -702,6 +706,9 @@ app.controller('BaseEditController', [
     					} else if (model.koulutuksenAlkamiskausi.uri.indexOf("_s") > -1) {
         					minY.setFullYear(vuosi, 7, 1);
         					maxY.setFullYear(vuosi, 11, 31);
+    					} else {
+            				model.isMinmax = false;
+            				return;
     					}
     					$scope.min = minY;
     					$scope.max = maxY;
