7e283ed4ceb44abba80f92523df033357812894b
Alexis Troberg <alexis.troberg@gofore.com>
2014-11-27 21:52:07 +0200

    Revert "Merkitse parent komotot poistetuksi tietokannassa"
    Koulutusinformaation indeksointi perustuu edelleen parent komotoihin,
    joten ei voida tehdä muutoksia niihiin...
    
    This reverts commit 145c06ab0b5c68e015517d0fce69682dd4b78d2b.

--- tarjonta-service/src/main/resources/db/migration/V097__poista-parent-komotot.sql
+++
@@ -1,9 +0,0 @@
-/**
-Vaadin toteutus loi parent komotoja jokaiselle erilliselle koulutustyypille (1 per organisaatio).
-Näitä ei enää tarvita Angularin puolella, joten merkitään parent komotot poistetuksi.
- */
-update koulutusmoduuli_toteutus
-set tila = 'POISTETTU'
-where alkamisvuosi is null
-and toteutustyyppi not like '%_VALMISTAVA'
-and tila != 'POISTETTU';
