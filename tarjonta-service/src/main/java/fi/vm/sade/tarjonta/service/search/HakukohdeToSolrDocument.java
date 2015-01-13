fa3b44e248d0ffb4a95a46b1515c7759430fe82e
Lauri Siponen <lauri.siponen@cybercom.com>
2015-01-13 15:34:46 +0200

    add koulutusmoduuliTyyppi to hakukohde solr document

--- tarjonta-service/src/main/java/fi/vm/sade/tarjonta/service/search/HakukohdeToSolrDocument.java
+++ tarjonta-service/src/main/java/fi/vm/sade/tarjonta/service/search/HakukohdeToSolrDocument.java
@@ -95,6 +95,7 @@ public class HakukohdeToSolrDocument implements Function<Long, List<SolrInputDoc
         addKoulutuslajit(hakukohdeDoc, hakukohde);
         addKoulutusAsteTyyppi(hakukohdeDoc, hakukohde);
         addToteutustyyppi(hakukohdeDoc, hakukohde);
+        addModuulityyppi(hakukohdeDoc, hakukohde);
         addPohjakoulutusvaatimus(hakukohdeDoc, hakukohde);
         addKohdejoukko(hakukohdeDoc, hakukohde);
         addDataFromKoulutus(hakukohdeDoc, hakukohde);
@@ -253,6 +254,14 @@ public class HakukohdeToSolrDocument implements Function<Long, List<SolrInputDoc
             KoulutusmoduuliToteutus koulutusmoduuliToteutus = hakukohde.getFirstKoulutus();
             add(hakukohdeDoc, KOULUTUSTYYPPI_URI, koulutusmoduuliToteutus.getToteutustyyppi().uri());
             add(hakukohdeDoc, TOTEUTUSTYYPPI_ENUM, koulutusmoduuliToteutus.getToteutustyyppi());
+        }
+    }
+
+    private void addModuulityyppi(SolrInputDocument hakukohdeDoc, Hakukohde hakukohde) {
+        
+        if (!hakukohde.getKoulutusmoduuliToteutuses().isEmpty()) {
+            KoulutusmoduuliToteutus koulutusmoduuliToteutus = hakukohde.getFirstKoulutus();
+            add(hakukohdeDoc, KOULUTUSMODUULITYYPPI_ENUM, koulutusmoduuliToteutus.getKoulutusmoduuli().getModuuliTyyppi());
         }
     }
 
