bd228b4d086a082882ec6b3bb3a3808d3704645d
Alexis Troberg <alexis.troberg@gofore.com>
2015-01-30 15:21:38 +0200

    Tuki ng-includea varten

--- tarjonta-app-angular/fb-flo.js
+++ tarjonta-app-angular/fb-flo.js
@@ -10,15 +10,43 @@ var server = flo(
         verbose: false,
         glob: [
             '**/*.js',
-            '**/*.css'
+            '**/*.css',
+            '**/*.html'
         ]
     },
     function resolver(filepath, callback) {
+
         callback({
             resourceURL: filepath,
             contents: fs.readFileSync(dirToWatch + '/' + filepath),
             update: function (_window, _resourceURL) {
-                angular.element('body').scope().$root.$digest();
+
+                console.log('update');
+
+                _resourceURL = _resourceURL.replace('.notFound', '');
+
+                var el = angular.element($("[data-ng-include=\"'" + _resourceURL + "'\"]")[0]);
+                if (el.length === 0) {
+                    el = angular.element($("[ng-include=\"'" + _resourceURL + "'\"]")[0]);
+                }
+                if (el.length === 0) {
+                    angular.element('body').scope().$root.$digest();
+                    console.log('No matching element found');
+                    return;
+                }
+
+                var $injector = angular.element('body').injector();
+                var $compile = $injector.get('$compile');
+                var $http = $injector.get('$http');
+
+                console.log('Element found', el.length);
+
+                $http.get(_resourceURL).success(function(contents) {
+                    var newHtml = $compile(contents)(el.scope());
+                    el.html(newHtml);
+                    angular.element('body').scope().$root.$digest();
+                });
+
                 console.log("Resource " + _resourceURL + " has just been updated with new content");
             }
         });
