aefc861b45aeb8bf3f644bdf3607c309f1f9f782
Alexis Troberg <alexis.troberg@gofore.com>
2015-09-28 15:59:48 +0300

    Käytä oikeata operaatiota

--- tarjonta-service/src/main/java/fi/vm/sade/tarjonta/service/impl/resources/v1/HakukohdeResourceImplV1.java
+++ tarjonta-service/src/main/java/fi/vm/sade/tarjonta/service/impl/resources/v1/HakukohdeResourceImplV1.java
@@ -1186,7 +1186,8 @@ public class HakukohdeResourceImplV1 implements HakukohdeV1Resource {
                     builder.setOperation(TarjontaOperation.PUBLISH);
                     break;
                 default:
-                    builder.add("updateTila", tila.name());
+                    builder.setOperation(TarjontaOperation.CHANGE_STATE);
+                    builder.add("newTila", tila.name());
                     break;
             }
 
