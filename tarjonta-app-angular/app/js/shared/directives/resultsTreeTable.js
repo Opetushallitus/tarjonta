Opetushallitus/tarjonta

commit 249cb69fc8b6289cdfc05c46f8ee22c4c1858154

haku list table is rendered using plain JS, which exposes the page to XSS. now some content is escaped.

tarjonta-app-angular/app/js/shared/directives/resultsTreeTable.js
@@ -81,11 +81,19 @@ app.directive('resultsTreeTable', function(LocalisationService, loadingService, 
                 return null;
             }
         }
+        function escapeHtml(unsafe) {
+            return unsafe
+                .replace(/&/g, "&amp;")
+                .replace(/</g, "&lt;")
+                .replace(/>/g, "&gt;")
+                .replace(/"/g, "&quot;")
+                .replace(/'/g, "&#039;");
+        }
         function getContent(row, col) {
             //console.log("DELEGATE getContent", $scope.getContent);
             var ret = $scope.getContent() && $scope.getContent()(row, col);
             try {
-                return ret === null || ret === undefined ? '' : ret;
+                return ret === null || ret === undefined ? '' : escapeHtml(ret);
             }
             catch (err) {
                 console.log(err);

commit 87f59d1da8f487f080145f759d7c196637bc0a4e

BUG-690 aloituspaikoissa näkyy tarjonnassa hakukohteilla "NULL"
Bug caused by 249cb69fc8b6289cdfc05c46f8ee22c4c1858154

Fix: convert "unsafe"-parameter to string before attempting string replace.

tarjonta-app-angular/app/js/shared/directives/resultsTreeTable.js
@@ -82,7 +82,7 @@ app.directive('resultsTreeTable', function(LocalisationService, loadingService, 
             }
         }
         function escapeHtml(unsafe) {
-            return unsafe
+            return ('' + unsafe)
                 .replace(/&/g, "&amp;")
                 .replace(/</g, "&lt;")
                 .replace(/>/g, "&gt;")