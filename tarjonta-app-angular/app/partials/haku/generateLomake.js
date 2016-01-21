Opetushallitus/tarjonta

commit 4d0bc211f75a8e9f0388dbc3cdd6f588af6b11fd

Haun lomakkeen generointi
Squashed commit of the following:

commit 798492a7a6a6cac95989849967a86f7e329f8234
Author: Alexis Troberg <alexis.troberg@gofore.com>
Date:   Thu Jan 7 13:35:45 2016 +0200

    Ignore system error dialog

commit b4148b1b9087e4f585529908fa2880ec12f7d0ee
Author: Alexis Troberg <alexis.troberg@gofore.com>
Date:   Thu Jan 7 11:35:28 2016 +0200

    Check that response is 200

commit 6197b7567ad88429cc3d1ce453675859646d0079
Author: Alexis Troberg <alexis.troberg@gofore.com>
Date:   Thu Jan 7 10:09:57 2016 +0200

    Not authorized error message

commit 5045d66c61fd5c532ed9201bcadb8784f1e22281
Author: Alexis Troberg <alexis.troberg@gofore.com>
Date:   Thu Dec 31 11:44:37 2015 +0200

    Haun lomakkeen generointi

tarjonta-app-angular/app/partials/haku/generateLomake.js
@@ -1,0 +1,30 @@
+var app = angular.module('app.koulutus.extend.ctrl', []);
+
+app.controller('GenerateLomakeController', function($modalInstance, hakuOid, $http, $scope, $injector) {
+    'use strict';
+
+    $scope.close = function() {
+        $modalInstance.dismiss();
+    };
+
+    $scope.generateLomake = function(oid) {
+        $scope.loading = true;
+
+        $http.post('/haku-app/generatelomake/one/' + oid)
+            .then(function success(response) {
+                $scope.loading = false;
+                $scope.success = response.status == 200;
+                $scope.error = !$scope.success;
+            }, function error(response) {
+                var loadingService = $injector.get('loadingService');
+                loadingService.onErrorHandled();
+
+                $scope.loading = false;
+                $scope.error = true;
+                $scope.permissionDenied = response.status == 403;
+            });
+    };
+
+    $scope.generateLomake(hakuOid);
+
+});

tarjonta-app-angular/config/jsFiles.js
@@ -118,6 +118,7 @@ module.exports = [
     'partials/valintaperustekuvaus/remove/poista-ctrl.js',
     'partials/koulutus/copy/copy-move-koulutus.js',
     'partials/koulutus/copy/extend-koulutus.js',
+    'partials/haku/generateLomake.js',
     'partials/haku/hakuControllers.js',
     'partials/haku/list/hakuListControllers.js',
     'partials/haku/list/hakuSearchParameters.js',

tarjonta-app-angular/app/css/app.css
@@ -969,3 +969,12 @@ table.tarjontaForm.liiteList th:after {
 table.checklist span, .liite.well label span {
     vertical-align: middle;
 }
+
+#generatelLomakeModal div.loading {
+    padding: 1em;
+}
+
+#generatelLomakeModal img.loading {
+    float: left;
+    margin: -.5em 1em 0 0;
+}

tarjonta-app-angular/app/partials/haku/generateLomake.html
@@ -1,0 +1,23 @@
+<div id="generatelLomakeModal">
+    <div class="modal-header">
+        <h3><span  tt="generateLomake.modal.title"></span></h3>
+    </div>
+
+    <div class="modal-body">
+        <div ng-if="loading">
+            <img class="loading" src="img/ajax-loader-big.gif">
+            <p tt="generateLomake.modal.loading"></p>
+        </div>
+        <div ng-if="success">
+            <p tt="generateLomake.modal.success"></p>
+        </div>
+        <div ng-if="error">
+            <p ng-if="!permissionDenied" tt="generateLomake.modal.error"></p>
+            <p ng-if="permissionDenied" tt="generateLomake.modal.error.permissionDenied"></p>
+        </div>
+    </div>
+
+    <div class="modal-footer">
+        <button class="btn btn-primary" ng-click="close()" tt="tarjonta.toiminnot.sulje">Sulje</button>
+    </div>
+</div>

tarjonta-app-angular/app/partials/haku/review/hakuReviewControllers.js
@@ -98,6 +98,17 @@ app.controller('HakuReviewController', function($scope, $route, $log, $routePara
             controller: 'HakuCopyController'
         });
     };
+    $scope.generateLomake = function() {
+        $modal.open({
+            templateUrl: 'partials/haku/generateLomake.html',
+            controller: 'GenerateLomakeController',
+            resolve: {
+                hakuOid: function() {
+                    return $scope.model.hakux.result.oid;
+                }
+            }
+        });
+    };
     function flattenHakukohteet(tulokset) {
         return _.reduce(tulokset, function(memo, orgGroup) {
             _.each(orgGroup.tulokset, function(hakukohde) {

tarjonta-app-angular/app/partials/haku/review/review.html
@@ -10,6 +10,7 @@
             <controls-button icon="arrow-left" tt-key="haku.review.takaisin" action="goBack($event)">Takaisin</controls-button>
             <controls-button disabled="!isRemovable" tt-key="haku.review.poista" action="doDelete($event)">Poista</controls-button>
             <controls-button disabled="!isCopyable" tt-key="haku.review.kopioi" action="doCopy()">Kopioi uudelle kaudelle</controls-button>
+            <controls-button disabled="!isMutable" tt-key="haku.review.generoiLomake" action="generateLomake()">Generoi haun lomake</controls-button>
             <controls-notify type="message" tt-expr="'haku.review.murupolku'" tt-params="[model.nimi]"></controls-notify>
 
         </controls-model>

commit ecb0fb9f3d35ba66ce73ca93aa72ced390d83859

Bug: app.koulutus.extend.ctrl was accidentally overwritten
Bug introduced in commit 4d0bc211f75a8e9f0388dbc3cdd6f588af6b11fd

tarjonta-app-angular/app/partials/haku/generateLomake.js
@@ -1,4 +1,4 @@
-var app = angular.module('app.koulutus.extend.ctrl', []);
+var app = angular.module('app.haku.ctrl');
 
 app.controller('GenerateLomakeController', function($modalInstance, hakuOid, $http, $scope, $injector) {
     'use strict';

tarjonta-app-angular/config/jsFiles.js
@@ -118,8 +118,8 @@ module.exports = [
     'partials/valintaperustekuvaus/remove/poista-ctrl.js',
     'partials/koulutus/copy/copy-move-koulutus.js',
     'partials/koulutus/copy/extend-koulutus.js',
-    'partials/haku/generateLomake.js',
     'partials/haku/hakuControllers.js',
+    'partials/haku/generateLomake.js',
     'partials/haku/list/hakuListControllers.js',
     'partials/haku/list/hakuSearchParameters.js',
     'partials/haku/edit/hakuEditControllers.js',