740461622bb793021600326c68acd687b6697c2f
Jani Rahkola <jani.rahkola@reaktor.com>
2016-09-19 15:38:13 +0300

    Format code and reason messages

--- tarjonta-service/src/main/java/fi/vm/sade/tarjonta/service/impl/resources/v1/util/YhdenPaikanSaantoBuilder.java
+++ tarjonta-service/src/main/java/fi/vm/sade/tarjonta/service/impl/resources/v1/util/YhdenPaikanSaantoBuilder.java
@@ -22,49 +22,50 @@ public class YhdenPaikanSaantoBuilder {
             return new YhdenPaikanSaanto(false, "Ei korkeakouluhaku");
         }
         boolean hasAlkamisVuosiAndUri = haku.getKoulutuksenAlkamisVuosi() != null && haku.getKoulutuksenAlkamiskausiUri() != null;
-        if(hasAlkamisVuosiAndUri) {
+        if (hasAlkamisVuosiAndUri) {
             boolean isBeforeSyksy2016 = haku.getKoulutuksenAlkamisVuosi() < 2016 ||
                     (haku.getKoulutuksenAlkamisVuosi() == 2016 && haku.getKoulutuksenAlkamiskausiUri().startsWith("kausi_k"));
             if (isBeforeSyksy2016) {
-                return new YhdenPaikanSaanto(false, "Koulutuksen alkamiskausi ennen syksyä 2016");
+                return new YhdenPaikanSaanto(false, "Haun koulutuksen alkamiskausi on ennen syksyä 2016");
             }
         } else {
-            return new YhdenPaikanSaanto(false, "Koulutuksella ei ole alkamisvuotta ja alkamiskautta");
+            return new YhdenPaikanSaanto(false, "Haulla ei ole koulutuksen alkamisvuotta tai alkamiskautta");
         }
-        if (haunKohdejoukontarkenneYPSYhteensopiva(haku)) {
-            if (StringUtils.isBlank(haku.getKohdejoukonTarkenne())) {
-                return new YhdenPaikanSaanto(true, "Korkeakouluhaku ilman kohdejoukon tarkennetta");
-            } else {
-                return new YhdenPaikanSaanto(true, String.format("Kohdejoukon tarkenne on '%s'", haku.getKohdejoukonTarkenne()));
-            }
+        if (!haunKohdejoukontarkenneYPSYhteensopiva(haku)) {
+            return new YhdenPaikanSaanto(false, String.format("Haulla on kohdejoukon tarkenne, joka ei ole joukossa %s",
+                    TARKENTEET_JOILLE_YHDEN_PAIKAN_SAANTO));
         }
-        return new YhdenPaikanSaanto(false, String.format("Kohdejoukon tarkenne on '%s', sääntö on voimassa tarkenteille %s",
-                haku.getKohdejoukonTarkenne(), TARKENTEET_JOILLE_YHDEN_PAIKAN_SAANTO));
+        if (StringUtils.isBlank(haku.getKohdejoukonTarkenne())) {
+            return new YhdenPaikanSaanto(true, "Korkeakouluhaku ilman kohdejoukon tarkennetta");
+        } else {
+            return new YhdenPaikanSaanto(true, String.format("Haun kohdejoukon tarkenne on '%s'",
+                    haku.getKohdejoukonTarkenne()));
+        }
     }
 
     public static YhdenPaikanSaanto from(Hakukohde hakukohde) {
         Haku haku = hakukohde.getHaku();
         YhdenPaikanSaanto yhdenPaikanSaantoBasedOnHaku = from(haku);
-        if(yhdenPaikanSaantoBasedOnHaku.isVoimassa()) {
+        if (yhdenPaikanSaantoBasedOnHaku.isVoimassa()) {
             return yhdenPaikanSaantoBasedOnHaku;
         }
-        if(haku.isKorkeakouluHaku()) {
+        if (haku.isKorkeakouluHaku()) {
             if (!haunKohdejoukontarkenneYPSYhteensopiva(haku)) {
                 return yhdenPaikanSaantoBasedOnHaku;
             }
-            if(haku.isJatkuva()) {
+            if (haku.isJatkuva()) {
                 Collection<KoulutusmoduuliToteutus> validKomotos = filterOnlyValidKomotos(hakukohde.getKoulutusmoduuliToteutuses());
-                if(validKomotos.isEmpty()) {
+                if (validKomotos.isEmpty()) {
                     return new YhdenPaikanSaanto(false, String.format("%s ja hakukohteella ei ole oikean tilaista koulutusmoduulia",
                             yhdenPaikanSaantoBasedOnHaku.getSyy()));
-                } else if(validKomotos.size() > 1) {
+                } else if (validKomotos.size() > 1) {
                     return new YhdenPaikanSaanto(false, String.format("%s ja hakukohteella on liian monta oikean tilaista koulutusmoduulia",
                             yhdenPaikanSaantoBasedOnHaku.getSyy()));
                 } else {
                     KoulutusmoduuliToteutus validKomoto = validKomotos.iterator().next();
                     boolean ennenSyksya2016 = validKomoto.getAlkamisVuosi() < 2016 ||
                             (validKomoto.getAlkamisVuosi() == 2016 && validKomoto.getAlkamiskausiUri().startsWith("kausi_k"));
-                    if(ennenSyksya2016) {
+                    if (ennenSyksya2016) {
                         return new YhdenPaikanSaanto(false, String.format("%s ja hakukohteen alkamiskausi ja vuosi on ennen syksyä 2016",
                                 yhdenPaikanSaantoBasedOnHaku.getSyy()));
                     } else {
@@ -82,15 +83,17 @@ public class YhdenPaikanSaantoBuilder {
     private static Collection<KoulutusmoduuliToteutus> filterOnlyValidKomotos(Set<KoulutusmoduuliToteutus> komotos) {
         return Collections2.filter(komotos, new Predicate<KoulutusmoduuliToteutus>() {
             private final List<TarjontaTila> INVALID = Arrays.asList(TarjontaTila.LUONNOS, TarjontaTila.KOPIOITU);
+
             @Override
             public boolean apply(@Nullable KoulutusmoduuliToteutus input) {
-                if(input == null) {
+                if (input == null) {
                     return false;
                 }
                 return !INVALID.contains(input.getTila()) && input.getAlkamisVuosi() != null && input.getAlkamiskausiUri() != null;
             }
         });
     }
+
     private static boolean haunKohdejoukontarkenneYPSYhteensopiva(Haku haku) {
         return StringUtils.isBlank(haku.getKohdejoukonTarkenne()) ||
                 TARKENTEET_JOILLE_YHDEN_PAIKAN_SAANTO.stream()
