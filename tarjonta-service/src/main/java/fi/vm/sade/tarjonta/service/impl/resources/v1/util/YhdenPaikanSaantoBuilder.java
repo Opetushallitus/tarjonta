fedb98b1a9796ad404ccb282bbb3f1a66096cc8a
Jani Rahkola <jani.rahkola@reaktor.com>
2016-09-19 16:01:54 +0300

    Inline filtering of koulutukset

--- tarjonta-service/src/main/java/fi/vm/sade/tarjonta/service/impl/resources/v1/util/YhdenPaikanSaantoBuilder.java
+++ tarjonta-service/src/main/java/fi/vm/sade/tarjonta/service/impl/resources/v1/util/YhdenPaikanSaantoBuilder.java
@@ -1,7 +1,5 @@
 package fi.vm.sade.tarjonta.service.impl.resources.v1.util;
 
-import com.google.common.base.Predicate;
-import com.google.common.collect.Collections2;
 import fi.vm.sade.tarjonta.model.Haku;
 import fi.vm.sade.tarjonta.model.Hakukohde;
 import fi.vm.sade.tarjonta.model.KoulutusmoduuliToteutus;
@@ -9,13 +7,14 @@ import fi.vm.sade.tarjonta.service.resources.v1.dto.HakuV1RDTO.YhdenPaikanSaanto
 import fi.vm.sade.tarjonta.shared.types.TarjontaTila;
 import org.apache.commons.lang.StringUtils;
 
-import javax.annotation.Nullable;
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class YhdenPaikanSaantoBuilder {
 
     private static final String JATKOTUTKINTOHAKU_URI = "haunkohdejoukontarkenne_3#";
     private static final List<String> TARKENTEET_JOILLE_YHDEN_PAIKAN_SAANTO = Collections.singletonList(JATKOTUTKINTOHAKU_URI);
+    private static final List<TarjontaTila> IGNORE_KOULUTUS_STATES = Arrays.asList(TarjontaTila.LUONNOS, TarjontaTila.KOPIOITU);
 
     public static YhdenPaikanSaanto from(Haku haku) {
         if (!haku.isKorkeakouluHaku()) {
@@ -56,22 +55,26 @@ public class YhdenPaikanSaantoBuilder {
                     TARKENTEET_JOILLE_YHDEN_PAIKAN_SAANTO
             ));
         }
-        Collection<KoulutusmoduuliToteutus> validKomotos = filterOnlyValidKomotos(hakukohde.getKoulutusmoduuliToteutuses());
-        if (validKomotos.isEmpty()) {
+        List<KoulutusmoduuliToteutus> koulutukset = hakukohde.getKoulutusmoduuliToteutuses().stream()
+                .filter(koulutus -> !IGNORE_KOULUTUS_STATES.contains(koulutus.getTila()) &&
+                        koulutus.getAlkamisVuosi() != null &&
+                        koulutus.getAlkamiskausiUri() != null)
+                .collect(Collectors.toList());
+        if (koulutukset.isEmpty()) {
             return new YhdenPaikanSaanto(false, String.format(
                     "%s ja hakukohteella ei ole oikean tilaista koulutusmoduulia",
                     ypsBasedOnHaku.getSyy()
             ));
         }
-        if (validKomotos.size() > 1) {
+        if (koulutukset.size() > 1) {
             return new YhdenPaikanSaanto(false, String.format(
                     "%s ja hakukohteella on liian monta oikean tilaista koulutusmoduulia",
                     ypsBasedOnHaku.getSyy()
             ));
         }
-        KoulutusmoduuliToteutus validKomoto = validKomotos.iterator().next();
-        boolean ennenSyksya2016 = validKomoto.getAlkamisVuosi() < 2016 ||
-                (validKomoto.getAlkamisVuosi() == 2016 && validKomoto.getAlkamiskausiUri().startsWith("kausi_k"));
+        KoulutusmoduuliToteutus koulutus = koulutukset.get(0);
+        boolean ennenSyksya2016 = koulutus.getAlkamisVuosi() < 2016 ||
+                (koulutus.getAlkamisVuosi() == 2016 && koulutus.getAlkamiskausiUri().startsWith("kausi_k"));
         if (ennenSyksya2016) {
             return new YhdenPaikanSaanto(false, String.format(
                     "%s ja hakukohteen alkamiskausi ja vuosi on ennen syksyä 2016",
@@ -80,20 +83,6 @@ public class YhdenPaikanSaantoBuilder {
         }
 
         return new YhdenPaikanSaanto(true, "Jatkuvan haun hakukohteen alkamiskausi ja vuosi on jälkeen kevään 2016");
-    }
-
-    private static Collection<KoulutusmoduuliToteutus> filterOnlyValidKomotos(Set<KoulutusmoduuliToteutus> komotos) {
-        return Collections2.filter(komotos, new Predicate<KoulutusmoduuliToteutus>() {
-            private final List<TarjontaTila> INVALID = Arrays.asList(TarjontaTila.LUONNOS, TarjontaTila.KOPIOITU);
-
-            @Override
-            public boolean apply(@Nullable KoulutusmoduuliToteutus input) {
-                if (input == null) {
-                    return false;
-                }
-                return !INVALID.contains(input.getTila()) && input.getAlkamisVuosi() != null && input.getAlkamiskausiUri() != null;
-            }
-        });
     }
 
     private static boolean haunKohdejoukontarkenneYPSYhteensopiva(Haku haku) {
