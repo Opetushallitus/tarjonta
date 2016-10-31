07ba3134345fb5dda6d88949bbba89ac8eb41593
Janne Sorvari <janne.sorvari@reaktor.fi>
2016-10-31 12:37:17 +0200

    Adjust Ataru url configuration
    * Add new base url for public Ataru

--- tarjonta-shared/src/main/java/fi/vm/sade/tarjonta/shared/UrlConfiguration.java
+++ tarjonta-shared/src/main/java/fi/vm/sade/tarjonta/shared/UrlConfiguration.java
@@ -12,5 +12,6 @@ public class UrlConfiguration extends OphProperties {
         addFiles("/tarjonta-service-oph.properties");
         addOptionalFiles(Paths.get(System.getProperties().getProperty("user.home"), "/oph-configuration/common.properties").toString());
         frontProperties.put("koulutusinformaatio-app-web.baseUrl", "https://${host.haku}");
+        frontProperties.put("ataru-app-web.baseUrl", "https://${host.haku}");
     }
 }
