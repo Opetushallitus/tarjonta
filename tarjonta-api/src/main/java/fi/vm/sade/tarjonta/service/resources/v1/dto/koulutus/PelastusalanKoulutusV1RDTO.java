70564473f69803ce5b626dd093f72bad7d0d31bb
Jaakko Salokanta <jaakko.salokanta@cybercom.com>
2017-01-16 10:23:49 +0200

    DTO extends Ammatillinen instead of Lukio dto

--- tarjonta-api/src/main/java/fi/vm/sade/tarjonta/service/resources/v1/dto/koulutus/PelastusalanKoulutusV1RDTO.java
+++ tarjonta-api/src/main/java/fi/vm/sade/tarjonta/service/resources/v1/dto/koulutus/PelastusalanKoulutusV1RDTO.java
@@ -16,7 +16,7 @@ package fi.vm.sade.tarjonta.service.resources.v1.dto.koulutus;
 
 import fi.vm.sade.tarjonta.shared.types.ToteutustyyppiEnum;
 
-public class PelastusalanKoulutusV1RDTO extends KoulutusLukioV1RDTO {
+public class PelastusalanKoulutusV1RDTO extends KoulutusAmmatillinenPerustutkintoV1RDTO {
 
   public PelastusalanKoulutusV1RDTO() {
     super(ToteutustyyppiEnum.PELASTUSALAN_KOULUTUS);
