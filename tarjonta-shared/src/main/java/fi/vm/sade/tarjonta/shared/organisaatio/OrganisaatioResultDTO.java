12b062938b8e6a46365a0606ac8eb24f266e042a
jussi.jartamo@gofore.com <jussi.jartamo@gofore.com>
2017-05-31 15:33:52 +0300

    oppilaitos from nearest parent or nearest child

--- tarjonta-shared/src/main/java/fi/vm/sade/tarjonta/shared/organisaatio/OrganisaatioResultDTO.java
+++ tarjonta-shared/src/main/java/fi/vm/sade/tarjonta/shared/organisaatio/OrganisaatioResultDTO.java
@@ -3,13 +3,10 @@ package fi.vm.sade.tarjonta.shared.organisaatio;
 import java.util.List;
 
 public class OrganisaatioResultDTO {
-    private List<OrganisaatioDTO> organisaatiot;
+    private List<OrganisaatioKelaDTO> organisaatiot;
 
-    public List<OrganisaatioDTO> getOrganisaatiot() {
+    public List<OrganisaatioKelaDTO> getOrganisaatiot() {
         return organisaatiot;
     }
 
-    public void setOrganisaatiot(List<OrganisaatioDTO> organisaatiot) {
-        this.organisaatiot = organisaatiot;
-    }
 }
