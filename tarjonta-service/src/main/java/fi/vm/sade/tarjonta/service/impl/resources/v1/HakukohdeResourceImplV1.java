51aa3c0a2c7a866babda91cdf01152d130117e8e
teo.mertanen <teo.mertanen@gofore.com>
2017-12-14 17:34:48 +0200

    BUG-1575 ei tarkisteta korkeakouluhakukohteitten tarjoajia

--- tarjonta-service/src/main/java/fi/vm/sade/tarjonta/service/impl/resources/v1/HakukohdeResourceImplV1.java
+++ tarjonta-service/src/main/java/fi/vm/sade/tarjonta/service/impl/resources/v1/HakukohdeResourceImplV1.java
@@ -1190,7 +1190,10 @@ public class HakukohdeResourceImplV1 implements HakukohdeV1Resource {
         permissionChecker.checkUpdateHakukohdeAndIgnoreParametersWhileChecking(hakukohde.getOid());
 
         List<HakukohdeValidationMessages> validationMessages = hakukohdeValidator.checkKoulutukset(getKomotoOids(koulutukses));
-        validationMessages.addAll(hakukohdeValidator.checkTarjoajat(hakukohde, koulutukses));
+
+        if (!hakukohde.getHaku().isKorkeakouluHaku()) {
+            validationMessages.addAll(hakukohdeValidator.checkTarjoajat(hakukohde, koulutukses));
+        }
 
         if (validationMessages.size() > 0) {
             return populateValidationErrors(null, validationMessages);
