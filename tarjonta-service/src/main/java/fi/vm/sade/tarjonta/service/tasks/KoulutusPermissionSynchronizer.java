f802c6586a3673e3ba9cdbea08751e127fb867b6
Petteri Turtiainen <petteri.turtiainen@cybercom.com>
2018-01-08 13:22:27 +0200

    REVERT ME! Oiva ajastus joka tunti.

--- tarjonta-service/src/main/java/fi/vm/sade/tarjonta/service/tasks/KoulutusPermissionSynchronizer.java
+++ tarjonta-service/src/main/java/fi/vm/sade/tarjonta/service/tasks/KoulutusPermissionSynchronizer.java
@@ -96,7 +96,7 @@ public class KoulutusPermissionSynchronizer {
     }
 
     @Transactional(readOnly = true)
-    @Scheduled(cron = "0 0 1 * * ?")
+    @Scheduled(cron = "0 0 * * * ?")
     public void checkExistingKoulutus() {
         LOG.info("Amkoute: check existing koulutus start");
 
