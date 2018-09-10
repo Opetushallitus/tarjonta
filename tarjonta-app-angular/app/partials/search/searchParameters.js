f872355faf7726d61ff6d905792e992aefafe4c8
Mikko Siukola <mikko.siukola@cybercom.com>
2018-09-10 16:18:15 +0300

    Poistettu organisaatiohaun kutsusta oletusparametrit organisaatio- ja oppilaitostyypeille

--- tarjonta-app-angular/app/partials/search/searchParameters.js
+++ tarjonta-app-angular/app/partials/search/searchParameters.js
@@ -100,8 +100,6 @@ angular.module('search.hakutulokset.searchparameters', [])
         function getDefaultHakuehdot() {
             return {
                 'searchStr': '',
-                'organisaatiotyyppi': '',
-                'oppilaitostyyppi': undefined,
                 'aktiiviset': true,
                 'lakkautetut': false,
                 'suunnitellut': false,
