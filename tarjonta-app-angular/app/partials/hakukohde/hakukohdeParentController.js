f416a71b153d09c84a6be6d44e2a75f7fa26ebd6
Tatu Honkanen <tatu.honkanen@cybercom.com>
2019-09-24 16:00:23 +0300

    BUG-2061 : edit comment.

--- tarjonta-app-angular/app/partials/hakukohde/hakukohdeParentController.js
+++ tarjonta-app-angular/app/partials/hakukohde/hakukohdeParentController.js
@@ -1394,7 +1394,7 @@ app.controller('HakukohdeParentController', [
                         });
                     });
                     $scope.checkIsCopy($scope.luonnosVal, true);
-                    // BUG-1108, BUG-1213
+                    // BUG-1108, BUG-1213, BUG-2061
                     ['hakuaikaAlkuPvm', 'hakuaikaLoppuPvm', 'aloituspaikatLkm', 'ensikertalaistenAloituspaikat', 'alinHyvaksyttavaKeskiarvo'].map(function(field) {
                         nullAsZero($scope.model.hakukohde, field);
                     });
